868b57b59024eadefadb63734cf3642f3ae13c0e
RajThakur777 <traj15019@gmail.com>
2025-01-01 08:18:29 +0530

    Time: 0 ms (100%), Space: 8.7 MB (38.26%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -1,28 +1,64 @@
-//Brute force:
+// //Brute force:
+// class Solution {
+// public:
+//     int maxScore(string s) {   
+//         int n = s.size();
+
+//         int maxScore = INT_MIN;
+
+//         for(int i=0; i<n-1; i++){
+//             int zero = 0;
+//             int one = 0;
+
+//             for(int left=0; left<=i; left++){
+//                 if(s[left] == '0'){
+//                     zero++;
+//                 }
+//             }
+
+//             for(int right=i+1; right<=n; right++){
+//                 if(s[right] == '1'){
+//                     one++;
+//                 }
+//             }
+//             maxScore = max(maxScore , zero + one);
+//         }
+//         return maxScore;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+//Optimised:
 class Solution {
 public:
     int maxScore(string s) {   
         int n = s.size();
 
-        int maxScore = INT_MIN;
+        int ones = 0;
+        for(int i=0; i<n; i++){
+            if(s[i] == '1') ones++;
+        }
+
+        int zeroes = 0;
+        int ans = INT_MIN;
 
         for(int i=0; i<n-1; i++){
-            int zero = 0;
-            int one = 0;
-            
-            for(int left=0; left<=i; left++){
-                if(s[left] == '0'){
-                    zero++;
-                }
+            if(s[i] == '1'){
+                ones--;
+            }
+            else {
+                zeroes++;
             }
 
-            for(int right=i+1; right<=n; right++){
-                if(s[right] == '1'){
-                    one++;
-                }
-            }
-            maxScore = max(maxScore , zero + one);
+            ans = max(ans , zeroes + ones);
         }
-        return maxScore;
+        return ans;
     }
 };
