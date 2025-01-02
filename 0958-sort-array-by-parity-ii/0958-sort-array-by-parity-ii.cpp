190aa4b2d84227bca82ace5ccc8a7822e0f08003
RajThakur777 <traj15019@gmail.com>
2025-01-02 18:44:12 +0530

    Time: 0 ms (100%), Space: 25.9 MB (49.49%) - LeetHub

--- 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.cpp
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.cpp
@@ -1,13 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> sortArrayByParityII(vector<int>& nums) {
+//         int n = nums.size();
+
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 if((nums[i] % 2 != (i % 2)) && (nums[j] % 2 != (j % 2))){
+//                     swap(nums[i] , nums[j]);
+//                 }
+//             }
+//         }
+//         return nums;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     vector<int> sortArrayByParityII(vector<int>& nums) {
         int n = nums.size();
 
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if((nums[i] % 2 != (i % 2)) && (nums[j] % 2 != (j % 2))){
-                    swap(nums[i] , nums[j]);
-                }
+        int i = 0;
+        int j = 1;
+
+        while(i < n && j < n){
+            if(nums[i] % 2 == 0){
+                i += 2;
+            }
+            else if(nums[j] % 2 != 0){
+                j += 2;
+            }
+            else {
+                swap(nums[i] , nums[j]);
             }
         }
         return nums;
