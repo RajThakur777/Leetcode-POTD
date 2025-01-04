c9554cba6f314d1c9e15dc5373f2a76c23062ae2
RajThakur777 <traj15019@gmail.com>
2025-01-04 23:27:12 +0530

    Time: 20 ms (5.79%), Space: 133.3 MB (49.37%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,58 @@
+// class Solution {
+// public:
+//     vector<int> pivotArray(vector<int>& nums, int pivot) {
+//         int n = nums.size();
+//         vector<int> ans(n , 0);
+
+//         int k = 0;
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] < pivot){
+//                 ans[k++] = nums[i];
+//             }
+//         }
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] == pivot){
+//                 ans[k++] = nums[i];
+//             }
+//         }
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] > pivot){
+//                 ans[k++] = nums[i];
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        vector<int> ans(n , 0);
-
-        int k = 0;
+        vector<int> ans;
 
         for(int i=0; i<n; i++){
             if(nums[i] < pivot){
-                ans[k++] = nums[i];
+                ans.push_back(nums[i]);
             }
         }
 
         for(int i=0; i<n; i++){
             if(nums[i] == pivot){
-                ans[k++] = nums[i];
+                ans.push_back(nums[i]);
             }
         }
 
         for(int i=0; i<n; i++){
             if(nums[i] > pivot){
-                ans[k++] = nums[i];
+                ans.push_back(nums[i]);
             }
         }
         return ans;
