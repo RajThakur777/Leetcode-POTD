25456ab4350261c35856c35dd5ba388353dcf6f4
RajThakur777 <traj15019@gmail.com>
2025-01-12 16:21:42 +0530

    Time: 0 ms (100%), Space: 26 MB (49.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,71 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n = height.size();
+
+//         int prefix[n];
+//         int suffix[n];
+
+//         prefix[0] = height[0];
+//         suffix[n-1] = height[n-1];
+
+//         for(int i=1; i<n; i++){
+//             prefix[i] = max(prefix[i-1] , height[i]);
+//         }
+
+//         for(int i=n-2; i>=0; i--){
+//             suffix[i] = max(suffix[i+1] , height[i]);
+//         }
+
+//         int ans = 0;
+//         for(int i=0; i<n; i++){
+//             ans += min(prefix[i] , suffix[i]) - height[i];
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
 class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
 
-        int prefix[n];
-        int suffix[n];
+        int left = 0;
+        int right = n-1;
 
-        prefix[0] = height[0];
-        suffix[n-1] = height[n-1];
-
-        for(int i=1; i<n; i++){
-            prefix[i] = max(prefix[i-1] , height[i]);
-        }
-
-        for(int i=n-2; i>=0; i--){
-            suffix[i] = max(suffix[i+1] , height[i]);
-        }
+        int leftMax = 0;
+        int rightMax = 0;
 
         int ans = 0;
-        for(int i=0; i<n; i++){
-            ans += min(prefix[i] , suffix[i]) - height[i];
+
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left] >= leftMax){
+                    leftMax = height[left];
+                }
+                else {
+                    ans += leftMax - height[left];
+                }
+
+                left++;
+            }
+            else {
+                if(height[right] <= height[left]){
+                    if(height[right] >= rightMax){
+                        rightMax = height[right];
+                    }
+                    else {
+                        ans += rightMax - height[right];
+                    }
+                    right--;
+                }
+            }
         }
         return ans;
     }
