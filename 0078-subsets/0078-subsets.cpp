d36c8e53b00cc91d82bb8f932e98c586f2eef080
RajThakur777 <traj15019@gmail.com>
2025-01-13 22:20:01 +0530

    Time: 0 ms (100%), Space: 9.9 MB (75.33%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
 
-    void recursion(vector<int> &nums , int idx , vector<int> &temp , vector<vector<int>> &ans){
+    void recursion(vector<int> &nums , int idx , vector<vector<int>> &ans , vector<int> &temp){
         if(idx == nums.size()){
             ans.push_back(temp);
             return;
         }
 
-        //take
+        //pick
         temp.push_back(nums[idx]);
-        recursion(nums , idx + 1 , temp , ans);
+        recursion(nums , idx + 1 , ans , temp);
         temp.pop_back();
 
-        //not-take
-        recursion(nums , idx + 1 , temp , ans);
+        //not-pick
+        recursion(nums , idx + 1 , ans , temp);
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) { 
+    vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
 
-        recursion(nums , 0 , temp , ans);   
+        recursion(nums , 0 , ans , temp); 
 
         return ans;
     }
