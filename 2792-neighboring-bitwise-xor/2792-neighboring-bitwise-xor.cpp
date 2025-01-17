46bba77c1db6383b80eeca1090f2cf0a910b30c0
RajThakur777 <traj15019@gmail.com>
2025-01-17 08:16:34 +0530

    Time: 4 ms (47.66%), Space: 263.9 MB (48.13%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -22,20 +22,40 @@
 
 
 
+// class Solution {
+// public:
+//     bool doesValidArrayExist(vector<int>& derived) {   
+//         int n = derived.size();
+        
+//         int xr = 0;
+
+//         for(int i=0; i<n; i++){
+//             xr ^= derived[i];
+//         }
+
+//         if(xr == 0){
+//             return true;
+//         }
+//         return false;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {   
         int n = derived.size();
         
-        int xr = 0;
+        int sum = 0;
 
-        for(int i=0; i<n; i++){
-            xr ^= derived[i];
-        }
+        for(int i=0; i<n; i++) sum += derived[i];
 
-        if(xr == 0){
-            return true;
-        }
+        if(sum % 2 == 0) return true;
+
         return false;
     }
 };
