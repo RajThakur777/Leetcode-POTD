163c340675cda5c22cbba13a90cdacae6d6ac047
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-01-19 17:56:34 +0530

    Time: 12 ms (47.56%), Space: 13.7 MB (39.04%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,3 +1,30 @@
+// class Solution {
+// public:
+//     int lengthOfLongestSubstring(string s) { 
+//         int n = s.size();
+
+//         int maxi = 0;
+//         int i = 0;
+
+//         map<char , int> mpp;
+//         for(int j=0; j<n; j++){
+//             mpp[s[j]]++;
+
+//             while(mpp[s[j]] > 1){
+//                 mpp[s[i]]--;
+//                 i++;
+//             }
+
+//             maxi = max(maxi , j - i + 1);
+//         }
+//         return maxi;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) { 
@@ -10,12 +37,17 @@ public:
         for(int j=0; j<n; j++){
             mpp[s[j]]++;
 
-            while(mpp[s[j]] > 1){
-                mpp[s[i]]--;
-                i++;
+            while(mpp.size() < j - i + 1){
+                if(mpp.size() < j - i + 1){
+                    mpp[s[i]]--;
+                    if(mpp[s[i]] == 0) mpp.erase(s[i]);
+                    i++;
+                }
             }
 
-            maxi = max(maxi , j - i + 1);
+            if(mpp.size() == j - i + 1){
+                maxi = max(maxi , j - i + 1);
+            }
         }
         return maxi;
     }
