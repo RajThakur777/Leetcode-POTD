7dbc9817dee0f4e326ea86451466579e840286d6
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-01-28 09:14:45 +0530

    Time: 15 ms (39.93%), Space: 96.6 MB (56.04%) - LeetHub

--- 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/2764-maximum-number-of-fish-in-a-grid.cpp
@@ -1,4 +1,3 @@
-//DFS:
 class Solution {
 public:
 
@@ -13,21 +12,21 @@ public:
         return grid[i][j] + dfs(i+1 , j , grid , visited) + dfs(i-1 , j , grid , visited) + dfs(i , j+1 , grid , visited) + dfs(i , j-1 , grid , visited);
     }
 
-    int findMaxFish(vector<vector<int>>& grid) {
+    int findMaxFish(vector<vector<int>>& grid) {      
         int m = grid.size();
         int n = grid[0].size();
 
-        int res = 0;
+        vector<vector<bool>> visited(m , vector<bool>(n , false));
 
-        vector<vector<bool>> visited(m , vector<bool> (n , false));
+        int ans = 0;
 
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j] > 0 && !visited[i][j]){
-                    res = max(res , dfs(i , j , grid , visited));
+                    ans = max(ans , dfs(i , j , grid , visited));
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
