f8fe5d3c47fc756d661a460ba066bde1dcaa3022
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-06 12:15:34 +0530

    Time: 849 ms (8.46%), Space: 99.4 MB (10.34%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -1,3 +1,29 @@
+// class Solution {
+// public:
+//     int tupleSameProduct(vector<int>& nums) {   
+//         int n = nums.size();
+
+//         map<int , int> mpp;
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 mpp[(nums[i] * nums[j])]++;
+//             }
+//         }
+
+//         int cnt = 0;
+//         for(auto it : mpp){
+//             int res = ((it.second) * (it.second - 1) ) / 2;
+
+//             cnt += (res) * 8;
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {   
@@ -12,9 +38,11 @@ public:
 
         int cnt = 0;
         for(auto it : mpp){
-            int res = ((it.second) * (it.second - 1) ) / 2;
+            if(it.second >= 2){
+                int res = ((it.second) * (it.second - 1) ) / 2;
 
-            cnt += (res) * 8;
+               cnt += (res) * 8;
+            }
         }
         return cnt;
     }
