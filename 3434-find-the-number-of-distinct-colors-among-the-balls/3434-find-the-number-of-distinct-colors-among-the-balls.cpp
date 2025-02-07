9b0772033b2f0b81d80c306dfe3fb68d40210f9f
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-07 10:51:45 +0530

    Time: 123 ms (44.69%), Space: 155.7 MB (66.56%) - LeetHub

--- 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/3434-find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,30 +1,31 @@
+//Using Two Hashmaps:
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
         int n = queries.size();
-        vector<int> result(n);
 
-        unordered_map<int, int> colormp; //color -> count
-        unordered_map<int, int> ballmp; //ball -> color
+        vector<int> ans(n);
 
-        for(int i = 0; i < n; i++) {
-            int ball  = queries[i][0];
+        unordered_map<int , int> mpp1;
+        unordered_map<int , int> mpp2;
+
+        for(int i=0; i<n; i++){
+            int ball = queries[i][0];
             int color = queries[i][1];
 
-            if(ballmp.count(ball)) { //already colored ball
-                int prevColor = ballmp[ball];
-                colormp[prevColor]--;
+            if(mpp1.find(ball) != mpp1.end()){
+                int prev = mpp1[ball];
+                mpp2[prev]--;
 
-                if(colormp[prevColor] == 0) {
-                    colormp.erase(prevColor);
+                if(mpp2[prev] == 0){
+                    mpp2.erase(prev);
                 }
             }
+            mpp1[ball] = color;
+            mpp2[color]++;
 
-            ballmp[ball] = color;
-            colormp[color]++;
-
-            result[i] = colormp.size();
+            ans[i] = mpp2.size();
         }
-        return result;
+        return ans;
     }
 };
