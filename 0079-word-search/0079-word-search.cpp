1a0e8aae270fb3a9636589a73c657ca3e0f983f1
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-08 11:12:34 +0530

    Time: 782 ms (38.53%), Space: 11.4 MB (55.55%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,41 +1,44 @@
 //Recursion:
 class Solution {
+public:
+ 
     int dx[4] = {0 , 0 , 1 , -1};
     int dy[4] = {1 , -1 , 0 , 0};
 
-public:
-    bool recursion(int r , int c , vector<vector<char>> &board , string word){
+    bool helper(vector<vector<char>>& board , int i , int j , string word){
         if(word.size() == 0) return true;
 
         int n = board.size();
         int m = board[0].size();
 
-        if(r >= n || r < 0 || c >= m || c < 0 || board[r][c] != word[0]) return false;
+        if(i < 0 || i >= n || j < 0 || j >= m || board[i][j] != word[0]) return false;
 
-        char ch = board[r][c];
-        board[r][c] = '*';
-
-        string s = word.substr(1);
+        char ch = board[i][j];
+        board[i][j] = '*';
         
-        bool ans = false;
-        for(int i=0; i<4; i++){
-           int nr = r + dx[i];
-           int nc = c + dy[i]; 
+        string s = word.substr(1);
 
-           ans |= recursion(nr , nc , board , s);
+        bool ans = 0;
+        for(int k=0; k<4; k++){
+            int nr = i + dx[k];
+            int nc = j + dy[k];
+
+            ans |= helper(board , nr , nc , s);
         }
-        board[r][c] = ch;
+        board[i][j] = ch;
+    
         return ans;
     }
 
-    bool exist(vector<vector<char>>& board, string word) {    
+    bool exist(vector<vector<char>>& board, string word) {
         int n = board.size();
         int m = board[0].size();
 
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(board[i][j] == word[0]){
-                    bool ans = recursion(i , j , board , word);
+                    bool ans = helper(board , i , j , word);
+
                     if(ans) return true;
                 }
             }
