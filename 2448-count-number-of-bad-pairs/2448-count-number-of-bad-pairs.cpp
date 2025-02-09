cfb2e9c4c082e842b57761e684ca97b00740cd83
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-09 13:06:17 +0530

    Time: 74 ms (33.74%), Space: 87.6 MB (70.06%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -1,21 +1,49 @@
+// class Solution {
+// public:
+//     long long countBadPairs(vector<int>& nums) {
+//         long long cnt = 0;
+
+//         int n =  nums.size();
+
+//         map<int , int> mpp;
+
+//         for(int i=0; i<n; i++){
+//             mpp[nums[i] - i]++;
+//         }
+
+//         long long total = (n * 1LL * (n - 1)) / 2;
+
+//         for(auto it : mpp){
+//             cnt += (it.second * 1LL * (it.second - 1)) / 2;
+//         }
+//         return total - cnt;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     long long countBadPairs(vector<int>& nums) {
+        int n = nums.size();
+
         long long cnt = 0;
 
-        int n =  nums.size();
+        unordered_map<int , int> mpp;
 
-        map<int , int> mpp;
+        mpp[nums[0]] = 1;
 
-        for(int i=0; i<n; i++){
+        for(int i=1; i<n; i++){
+            int elementsBefore = i;
+            int equalElements = mpp[nums[i] - i];
+
+            cnt += (elementsBefore - equalElements);
+
             mpp[nums[i] - i]++;
         }
-
-        long long total = (n * 1LL * (n - 1)) / 2;
-
-        for(auto it : mpp){
-            cnt += (it.second * 1LL * (it.second - 1)) / 2;
-        }
-        return total - cnt;
+        return cnt;
     }
 };
