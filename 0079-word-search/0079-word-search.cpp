22fb08aad4461df5dfd59fc9b452a27a1adaeed8
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-10 18:16:17 +0530

    Time: 757 ms (39.98%), Space: 11.6 MB (35.58%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,48 +1,48 @@
-//Recursion:
 class Solution {
 public:
- 
-    int dx[4] = {0 , 0 , 1 , -1};
-    int dy[4] = {1 , -1 , 0 , 0};
+    int dx[4] = {-1 , 0 , 1 , 0};
+    int dy[4] = {0 , 1 , 0 , -1};
 
-    bool helper(vector<vector<char>>& board , int i , int j , string word){
+    bool helper(int r , int c , vector<vector<char>>& board , string word) {
         if(word.size() == 0) return true;
 
-        int n = board.size();
-        int m = board[0].size();
+        int m = board.size();
+        int n = board[0].size();
+    
+        if(r < 0 || r >= m || c < 0 || c >= n || board[r][c] != word[0]) return false;
 
-        if(i < 0 || i >= n || j < 0 || j >= m || board[i][j] != word[0]) return false;
-
-        char ch = board[i][j];
-        board[i][j] = '*';
-        
-        string s = word.substr(1);
+        char ch = board[r][c];
+        board[r][c] = '*';
+        string str = word.substr(1);
 
         bool ans = 0;
-        for(int k=0; k<4; k++){
-            int nr = i + dx[k];
-            int nc = j + dy[k];
 
-            ans |= helper(board , nr , nc , s);
+        for(int i=0; i<4; i++){
+            int nr = r + dx[i];
+            int nc = c + dy[i];
+
+            ans |= helper(nr , nc , board , str);
         }
-        board[i][j] = ch;
-    
+        board[r][c] = ch;
+
         return ans;
     }
 
-    bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
+    bool exist(vector<vector<char>>& board, string word) { 
+        int m = board.size();
+        int n = board[0].size();
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
                 if(board[i][j] == word[0]){
-                    bool ans = helper(board , i , j , word);
+                    bool ans = helper(i , j , board , word);
 
-                    if(ans) return true;
+                    if(ans == true){
+                        return true;
+                    }
                 }
             }
-        }
+        } 
         return false;
     }
 };
