291becf0b8257c0476761de83322dcc5bafa0886
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-10 08:22:02 +0530

    Time: 4 ms (5.14%), Space: 9.3 MB (9.61%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,18 +1,42 @@
 class Solution {
 public:
-    string clearDigits(string& s) {
-        int k = 0;
 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] > '9') {
-                s[k++] = s[i];
-            }
-            else {
-                --k;
+    bool isDigit(char ch){
+        if(ch >= '0' && ch <= '9'){
+            return true;
+        }
+        return false;
+    }
+
+    bool isCharacter(char ch){
+        if(ch >= 'a' && ch <= 'z'){
+            return true;
+        }
+        return false;
+    }
+
+    string clearDigits(string s) {
+        int n = s.size();
+
+        string ans;
+
+        vector<int> mark(n , -1);
+        for(int i=0; i<n; i++){
+            if(isDigit(s[i])){
+                mark[i] = 0;
+                for(int j=i-1; j>=0; j--){
+                    if(isCharacter(s[j]) && mark[j] != 0){
+                        mark[j] = 0;
+                        break;
+                    }
+                }
             }
         }
-
-        s.resize(k);
-        return move(s);
+        for(int i=0; i<n; i++){
+            if(mark[i] == -1){
+                ans += s[i];
+            }
+        }
+        return ans;
     }
 };
