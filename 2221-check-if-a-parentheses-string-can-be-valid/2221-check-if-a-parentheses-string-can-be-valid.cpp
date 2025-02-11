b219ef105fbe0e5397aac235b1e4eb9a2b117817
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-11 19:31:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.cpp
@@ -1,36 +1,35 @@
-//Without using stack - constant space:
 class Solution {
 public:
     bool canBeValid(string s, string locked) {
         int n = s.size();
- 
+
         if(n % 2 != 0) return false;
 
         int open = 0;
+        int close = 0;
+
+
         for(int i=0; i<n; i++){
-            if(s[i] == '(' || locked[i] == '0'){
+            if(s[i] == '(' || locked[i] == 0){
                 open++;
             }
-            else if(s[i] == ')' && locked[i] == '1'){
+            else if(s[i] == ')' && locked[i] == 1){
                 open--;
             }
 
             if(open < 0) return false;
         }
 
-        int close = 0;
         for(int i=n-1; i>=0; i--){
-            if(s[i] == ')' || locked[i] == '0'){
+            if(s[i] == ')' || locked[i] == 0){
                 close++;
             }
-            else if(s[i] == '(' && locked[i] == '1'){
+            else if(s[i] == '(' && locked[i] == 1){
                 close--;
             }
 
             if(close < 0) return false;
         }
-
-
         return true;
     }
 };
