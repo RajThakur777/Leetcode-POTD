5875685af33c7fe887a563d6f01bb97d065dc6cf
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-11 16:57:52 +0530

    Time: 9 ms (8.99%), Space: 20.6 MB (7.85%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,26 @@
+//Recursion:
+class Solution {
+public:
+
+    void helper(int idx , int n , int numOpen , string curr , vector<string> &ans){
+        if(idx == n){
+            if(numOpen == 0){
+                ans.push_back(curr);
+            }
+            return;
+        }
+
+        helper(idx+1 , n , numOpen+1 , curr + "(" , ans);
+        if(numOpen){
+            helper(idx+1 , n , numOpen-1, curr+")" , ans);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+
+        helper(0 , 2*n , 0 , "" , ans);
+
+        return ans;
+    }
+};
