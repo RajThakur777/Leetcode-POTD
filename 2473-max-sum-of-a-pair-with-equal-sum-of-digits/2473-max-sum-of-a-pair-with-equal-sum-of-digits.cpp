0c0e14e1a85bac3e0ad979f9e720e4b15c9cdbb4
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-12 09:08:21 +0530

    Time: 45 ms (29.53%), Space: 63.7 MB (95.95%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,3 +1,43 @@
+// class Solution {
+// public:
+
+//     int digitSum(int n){
+//         int sum = 0;
+
+//         while(n > 0){
+//             sum += (n % 10);
+//             n = n / 10;
+//         }
+//         return sum;
+//     }
+
+//     int maximumSum(vector<int>& nums) {
+//         int n = nums.size();
+
+//         map<int , vector<int>> mpp;
+
+//         for(int i=0; i<n; i++){
+//             int num = digitSum(nums[i]);
+
+//             mpp[num].push_back(nums[i]);
+//         }
+
+//         int maxi = -1;
+//         for(auto &it : mpp){
+//             sort(it.second.begin() , it.second.end());
+
+//             if(it.second.size() >= 2){
+//                 maxi = max(maxi , it.second[it.second.size() - 1] + it.second[it.second.size() - 2]);
+//             }
+//         }
+//         return maxi;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
 
@@ -14,21 +54,17 @@ public:
     int maximumSum(vector<int>& nums) {
         int n = nums.size();
 
-        map<int , vector<int>> mpp;
-
-        for(int i=0; i<n; i++){
-            int num = digitSum(nums[i]);
-
-            mpp[num].push_back(nums[i]);
-        }
+        map<int , int> mpp;
 
         int maxi = -1;
-        for(auto &it : mpp){
-            sort(it.second.begin() , it.second.end());
+        for(int i=0; i<n; i++){
+            int sum = digitSum(nums[i]);
 
-            if(it.second.size() >= 2){
-                maxi = max(maxi , it.second[it.second.size() - 1] + it.second[it.second.size() - 2]);
+            if(mpp.count(sum)){
+                maxi = max(maxi , mpp[sum] + nums[i]);
             }
+
+            mpp[sum] = max(mpp[sum] , nums[i]);
         }
         return maxi;
     }
