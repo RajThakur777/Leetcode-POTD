9cce12de624e9f6b8d5981d6796c3cdf8b7f438f
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-13 08:05:52 +0530

    Time: 6 ms (0.86%), Space: 25.2 MB (3.82%) - LeetHub

--- 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3331-minimum-operations-to-exceed-threshold-value-i/3331-minimum-operations-to-exceed-threshold-value-i.cpp
@@ -1,15 +1,49 @@
+// class Solution {
+// public:
+//     int minOperations(vector<int>& nums, int k) {
+//         int n = nums.size();
+
+//         int cnt = 0;
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] < k){
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
         int n = nums.size();
 
-        int cnt = 0;
+        priority_queue <int, vector<int>, greater<int>> pq;
 
         for(int i=0; i<n; i++){
-            if(nums[i] < k){
-                cnt++;
-            }
+            pq.push(nums[i]);
+        }
+
+        int cnt = 0;
+
+        while(pq.top() < k){
+            pq.pop();
+            cnt++;
         }
         return cnt;
     }
 };
+
+
+
+
+
+
+
+
