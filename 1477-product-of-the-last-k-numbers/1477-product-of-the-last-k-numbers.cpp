86bd2797822b01d6da10508eedec63db7ade5aab
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-14 14:16:04 +0530

    Time: 1018 ms (20.85%), Space: 78.1 MB (43.27%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,3 +1,42 @@
+// class ProductOfNumbers {
+// public:
+//     vector<int> ans;
+//     ProductOfNumbers() {
+        
+//     }
+    
+//     void add(int num) {
+//         ans.push_back(num);
+//     }
+    
+//     int getProduct(int k) {
+//         int res = 1;
+//         int cnt = 1;
+//         for(int i=ans.size()-1; i>=0; i--){
+//             if(cnt <= k){
+//                 res *= ans[i];
+//                 cnt++;
+//             }
+//             else {
+//                 break;
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+// /**
+//  * Your ProductOfNumbers object will be instantiated and called as such:
+//  * ProductOfNumbers* obj = new ProductOfNumbers();
+//  * obj->add(num);
+//  * int param_2 = obj->getProduct(k);
+//  */
+
+
+
+
+
+
 class ProductOfNumbers {
 public:
     vector<int> ans;
@@ -11,15 +50,8 @@ public:
     
     int getProduct(int k) {
         int res = 1;
-        int cnt = 1;
-        for(int i=ans.size()-1; i>=0; i--){
-            if(cnt <= k){
-                res *= ans[i];
-                cnt++;
-            }
-            else {
-                break;
-            }
+        for(int i=ans.size()-k; i<ans.size(); i++){
+            res *= ans[i];
         }
         return res;
     }
