d972432134121e0361facd2a303694016233f9e4
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-14 18:14:15 +0530

    Time: 0 ms (100%), Space: 9.2 MB (56.92%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,51 +1,49 @@
+// //Recursion:
 // class Solution {
 // public:
 
-//     int recursion(int i , int j , int m , int n){
-//         if(i >= m || j >= n){
-//             return 0;
-//         }
+//     int helper(int i , int j , int m , int n){
 
-//         if(i == m-1 && j == n-1){
-//             return 1;
-//         }
+//         if(i == m-1 && j == n-1) return 1;
 
-//         return recursion(i + 1 , j , m , n) + recursion(i , j + 1 , m , n);
+//         if(i >= m || j >= n) return 0;
+
+//         return helper(i+1 , j , m , n) + helper(i , j+1 , m , n);
 //     }
 
 //     int uniquePaths(int m, int n) {
-//         return recursion(0 , 0 , m , n);
+//         int ans = helper(0 , 0 , m , n);
+
+//         return ans;
 //     }
 // };
 
 
 
 
-
-
+//Memoization:
+//Recursion:
 class Solution {
 public:
-    int recursion(int i , int j , int m , int n , vector<vector<int>> &dp){
-        if(i >= m || j >= n){
-            return 0;
-        }
+    int helper(int i , int j , int m , int n , vector<vector<int>> &dp){
+        if(i >= m || j >= n) return 0;
 
-        if(i == m-1 && j == n-1){
-            return 1;
-        }
+        if(i == m-1 && j == n-1) return 1;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[i][j] = recursion(i + 1 , j , m , n , dp) + recursion(i , j + 1 , m , n , dp);
+        return dp[i][j] = helper(i+1 , j , m , n , dp) + helper(i , j+1 , m , n , dp);
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m , vector<int>(n));
+        vector<vector<int>> dp(m , vector<int> (n));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 dp[i][j] = -1;
             }
         }
-        return recursion(0 , 0 , m , n , dp);
+        int ans = helper(0 , 0 , m , n , dp);
+
+        return ans;
     }
 };
