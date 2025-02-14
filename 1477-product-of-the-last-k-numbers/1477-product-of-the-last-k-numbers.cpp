3d16457cde6267dd7d6563713fd9d109652a618c
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-14 14:27:40 +0530

    Time: 15 ms (85.88%), Space: 78 MB (56.86%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -37,23 +37,73 @@
 
 
 
+// class ProductOfNumbers {
+// public:
+//     vector<int> ans;
+//     ProductOfNumbers() {
+        
+//     }
+    
+//     void add(int num) {
+//         ans.push_back(num);
+//     }
+    
+//     int getProduct(int k) {
+//         int res = 1;
+//         for(int i=ans.size()-k; i<ans.size(); i++){
+//             res *= ans[i];
+//         }
+//         return res;
+//     }
+// };
+
+// /**
+//  * Your ProductOfNumbers object will be instantiated and called as such:
+//  * ProductOfNumbers* obj = new ProductOfNumbers();
+//  * obj->add(num);
+//  * int param_2 = obj->getProduct(k);
+//  */
+
+
+
+
+
+
+
 class ProductOfNumbers {
 public:
-    vector<int> ans;
+    vector<int> nums;
+    int n;
     ProductOfNumbers() {
-        
+        nums.clear();
+        n = 0;
     }
     
     void add(int num) {
-        ans.push_back(num);
+        if(num == 0) {
+            nums = {};
+            n = 0;
+        }
+        else {
+            if(nums.empty()){
+                nums.push_back(num);
+            }
+            else {
+                nums.push_back(nums[n-1] * num);
+            }
+            n++;
+        }
     }
     
     int getProduct(int k) {
-        int res = 1;
-        for(int i=ans.size()-k; i<ans.size(); i++){
-            res *= ans[i];
+        if(k > n){
+            return 0;
         }
-        return res;
+        if(k == n){
+            return nums[n-1];
+        }
+
+        return (nums[n-1]) / nums[n-k-1];
     }
 };
 
