2f45a16d367b722fb74060442219a9bd6f0738a5
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-15 11:35:19 +0530

    Time: 10 ms (37.39%), Space: 15.5 MB (93.59%) - LeetHub

--- 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -1,3 +1,46 @@
+// class Solution {
+// public:
+//     int minInsertions(string s) {
+//         int n = s.size();
+
+//         int insertions = 0;
+//         int open = 0;
+
+//         for(int i=0; i<n; i++){
+//             if(s[i] == '('){
+//                 open++;
+//             }
+//             else if(s[i] == ')'){
+//                 if(i < n-1 && s[i+1] == ')'){
+//                     if(open > 0){
+//                         open--;
+//                     }
+//                     else {
+//                         insertions++;
+//                     }
+//                     i++;
+//                 }
+//                 else {
+//                     if(open > 0){
+//                         open--;
+//                         insertions++;
+//                     }
+//                     else if(open == 0){
+//                       insertions += 2;  
+//                     }
+//                 }
+//             }
+//         }
+//         return insertions + (open * 2);
+//     }
+// };
+
+
+
+
+
+
+
 class Solution {
 public:
     int minInsertions(string s) {
@@ -21,12 +64,13 @@ public:
                     i++;
                 }
                 else {
+                    insertions++;
+
                     if(open > 0){
                         open--;
-                        insertions++;
                     }
-                    else if(open == 0){
-                      insertions += 2;  
+                    else {
+                        insertions++;
                     }
                 }
             }
