3b8a17e028c2fc2537d4e0d5cd9ea4fcc57d8153
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-15 11:44:31 +0530

    Time: 5 ms (66.45%), Space: 15.5 MB (77.14%) - LeetHub

--- 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.cpp
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -50,31 +50,45 @@ public:
         int open = 0;
 
         for(int i=0; i<n; i++){
+            //cnt opening bracket
             if(s[i] == '('){
                 open++;
             }
+            //if i get closing bracket
             else if(s[i] == ')'){
+                //if i get two consecutive closing bracket
                 if(i < n-1 && s[i+1] == ')'){
+                    //look for an opening bracket before
                     if(open > 0){
+                        //if present pair it
                         open--;
                     }
                     else {
+                        //otherwise we need to insert an opening bracket
                         insertions++;
                     }
+                    //skip idx and move to next one as there we two consectutive closing brackets
                     i++;
                 }
+                //if we having opening bracket after closing bracket
                 else {
+                    //we need to insert an closing bracket as we want two closing bracket
                     insertions++;
 
+                    //then check same two conditions
+                    //if we have opening bracket before
                     if(open > 0){
+                        //pair it with two consecutive closing brackets
                         open--;
                     }
                     else {
+                        //otherwise add an opening bracket
                         insertions++;
                     }
                 }
             }
         }
+        //final answer will be insertions cnt + if we have x opening brackets left so we have to add 2*x brackets (closing)
         return insertions + (open * 2);
     }
 };
