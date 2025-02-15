7eb62853afde0dc3d2eeadf097cb674754962ae7
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-15 10:37:59 +0530

    Time: 3 ms (86.54%), Space: 15.6 MB (54.91%) - LeetHub

---
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/1648-minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n = s.size();
+
+        int insertions = 0;
+        int open = 0;
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '('){
+                open++;
+            }
+            else if(s[i] == ')'){
+                if(i < n-1 && s[i+1] == ')'){
+                    if(open > 0){
+                        open--;
+                    }
+                    else {
+                        insertions++;
+                    }
+                    i++;
+                }
+                else {
+                    if(open > 0){
+                        open--;
+                        insertions++;
+                    }
+                    else if(open == 0){
+                      insertions += 2;  
+                    }
+                }
+            }
+        }
+        return insertions + (open * 2);
+    }
+};
