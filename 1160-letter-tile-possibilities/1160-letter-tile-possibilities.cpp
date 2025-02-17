e50d0c3da53c190c8308452c0fa54ef8d6d8f794
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-17 15:13:55 +0530

    Time: 43 ms (33.33%), Space: 18 MB (30.04%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.cpp
@@ -1,26 +1,61 @@
 //Recursion + Backtracking:
+// class Solution {
+// public:
+
+//     void helper(string tiles , unordered_set<string> &st , string curr , vector<bool> &used){
+//         st.insert(curr);
+
+//         for(int i=0; i<tiles.size(); i++){
+//             if(!used[i]){
+//                 used[i] = true;
+//                 helper(tiles , st , curr + tiles[i] , used);
+//                 used[i] = false;
+//             }
+//         }
+//     }
+
+//     int numTilePossibilities(string tiles) { 
+//         unordered_set<string> st;
+
+//         vector<bool> used(tiles.size()+1 , false);
+
+//         helper(tiles , st , " " , used);
+
+//         return st.size() - 1; 
+//     }
+// };
+
+
+
+
+
+
+//Recursion + Backtracking:
 class Solution {
 public:
 
-    void helper(string tiles , unordered_set<string> &st , string curr , vector<bool> &used){
+    void helper(string tiles , string curr , unordered_set<string> &st , vector<bool> &used){
         st.insert(curr);
 
         for(int i=0; i<tiles.size(); i++){
-            if(!used[i]){
-                used[i] = true;
-                helper(tiles , st , curr + tiles[i] , used);
-                used[i] = false;
+            if(used[i] == true){
+                continue;
             }
+            used[i] = true;
+            helper(tiles , curr + tiles[i] , st , used);
+            used[i] = false;
         }
     }
 
     int numTilePossibilities(string tiles) { 
+        int n = tiles.size();
+
         unordered_set<string> st;
 
-        vector<bool> used(tiles.size()+1 , false);
+        vector<bool> used(n+1 , false);
 
-        helper(tiles , st , " " , used);
+        helper(tiles , " " , st , used);
 
-        return st.size() - 1; 
+        return st.size() - 1;
     }
 };
