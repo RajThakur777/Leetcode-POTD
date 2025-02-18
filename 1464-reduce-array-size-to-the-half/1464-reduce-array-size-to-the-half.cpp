4feffeb97713b81cb56f2ed0e27c970101780b04
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-18 09:48:52 +0530

    Time: 149 ms (18.93%), Space: 83.9 MB (38.41%) - LeetHub

---
+++ 1464-reduce-array-size-to-the-half/1464-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        int n = arr.size();
+
+        map<int , int> mpp;
+        for(int i=0; i<n; i++){
+            mpp[arr[i]]++;
+        }
+
+        priority_queue<int> pq;
+
+        for(auto [val , cnt] : mpp){
+            pq.push(cnt);
+        }
+
+        int ans = 0;
+        int sz = (n / 2);
+
+        while(sz > 0){
+            ans++;
+            sz -= pq.top();
+            pq.pop();
+        }
+        return ans;
+    }
+};
