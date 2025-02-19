badef97fb27406ad9fcc37fc82f0164adfcd5b0c
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-19 10:45:48 +0530

    Time: 4 ms (37.22%), Space: 11 MB (58.76%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,3 +1,37 @@
+// class Solution {
+// public:
+//     string frequencySort(string s) {
+//         int n = s.size();
+
+//         map<char , int> mpp;
+
+//         for(int i=0; i<n; i++){
+//             mpp[s[i]]++;
+//         }
+
+//         priority_queue<pair<int , char>> pq;
+//         for(auto it : mpp){
+//             pq.push({it.second , it.first});
+//         }
+
+//         string ans = "";
+//         while(!pq.empty()){
+//             char ch = pq.top().second;
+//             int freq = pq.top().first;
+
+//             pq.pop();
+
+//             ans.append(freq , ch);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
 class Solution {
 public:
     string frequencySort(string s) {
@@ -21,7 +55,10 @@ public:
 
             pq.pop();
 
-            ans.append(freq , ch);
+            while(freq > 0){
+                ans += ch;
+                freq--;
+            }
         }
         return ans;
     }
