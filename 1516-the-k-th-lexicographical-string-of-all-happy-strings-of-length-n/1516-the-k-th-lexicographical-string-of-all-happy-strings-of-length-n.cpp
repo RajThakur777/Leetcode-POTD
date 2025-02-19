3211bf0616be3ce892e20de1b1f45ff0fafed00c
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-19 08:49:07 +0530

    Time: 27 ms (27.8%), Space: 25.5 MB (28.59%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,3 +1,39 @@
+// //Recursion:
+// class Solution {
+// public:
+
+//     void helper(int idx , int n , vector<string> &ans , string curr , int k){
+//         if(idx == n){
+//             ans.push_back(curr);
+//             return;
+//         }
+
+//         for(char ch='a'; ch<='c'; ch++){
+//             if(curr.size() > 0 && curr.back() == ch) continue;
+
+//             helper(idx+1 , n , ans , curr+ch , k);
+//         }
+//     }
+
+//     string getHappyString(int n, int k) {
+//         vector<string> ans;
+
+//         helper(0 , n , ans , "" , k);
+
+//         sort(ans.begin() , ans.end());
+
+//         if(ans.size() < k){
+//             return "";
+//         }
+//         return ans[k-1];
+//     }
+// };
+
+
+
+
+
+
 //Recursion:
 class Solution {
 public:
@@ -19,8 +55,6 @@ public:
         vector<string> ans;
 
         helper(0 , n , ans , "" , k);
-
-        sort(ans.begin() , ans.end());
 
         if(ans.size() < k){
             return "";
