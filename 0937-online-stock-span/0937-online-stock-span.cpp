5d0bae68346893f31e5cd4007d49928e4388cfca
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-22 11:29:44 +0530

    Time: 29 ms (67.43%), Space: 94.1 MB (8.27%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,21 +1,56 @@
+// class StockSpanner {
+// public:
+
+//     stack<pair<int , int>> st; //stores {price , span}
+
+//     StockSpanner() {
+        
+//     }
+    
+//     int next(int price) {
+//         int span = 1;
+//         while(!st.empty() && price >= st.top().first){
+//             span += st.top().second;
+//             st.pop();
+//         }
+//         st.push({price , span});
+
+//         return st.top().second;
+//     }
+// };
+
+// /**
+//  * Your StockSpanner object will be instantiated and called as such:
+//  * StockSpanner* obj = new StockSpanner();
+//  * int param_1 = obj->next(price);
+//  */
+
+
+
+
+
 class StockSpanner {
 public:
 
-    stack<pair<int , int>> st; //stores {price , span}
+    stack<int> st;  //stores idx
+    vector<int> ans; 
 
     StockSpanner() {
         
     }
     
     int next(int price) {
-        int span = 1;
-        while(!st.empty() && price >= st.top().first){
-            span += st.top().second;
+        int i = ans.size();
+        ans.push_back(price);
+
+        while(!st.empty() && price >= ans[st.top()]){
             st.pop();
         }
-        st.push({price , span});
+        int span = st.empty() ? i+1 : i-st.top();
 
-        return st.top().second;
+        st.push(i);
+
+        return span;
     }
 };
 
