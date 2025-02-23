0fff76b369e35af62bd3c050a8a2d3c7f8c834fd
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-23 11:32:44 +0530

    Time: 7 ms (30.43%), Space: 27.1 MB (83.18%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,3 +1,55 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// //Recursion:
+// class Solution {
+// public:
+
+//     TreeNode* helper(vector<int>& inorder , vector<int>& postorder , int start , int end , int &idx){
+//         if(idx < 0 || start > end) return NULL;
+
+//         int rootVal = postorder[idx];
+
+//         int valueIdx = 0;
+//         for(int i=start; i<=end; i++){
+//             if(inorder[i] == rootVal){
+//                 valueIdx = i;
+//                 break;
+//             }
+//         }
+//         idx--;
+
+//         TreeNode* root = new TreeNode(rootVal);
+
+//         root->right = helper(inorder , postorder , valueIdx+1 , end , idx);
+//         root->left = helper(inorder , postorder , start , valueIdx-1 , idx);
+
+//         return root;
+//     }
+
+//     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+//         int n = postorder.size();
+
+//         int idx = n-1;
+
+//         return helper(inorder , postorder , 0 , n-1 , idx);
+//     }
+// };
+
+
+
+
+
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -18,10 +70,9 @@ public:
 
         int rootVal = postorder[idx];
 
-        int valueIdx = 0;
-        for(int i=start; i<=end; i++){
+        int i;
+        for(i=start; i<=end; i++){
             if(inorder[i] == rootVal){
-                valueIdx = i;
                 break;
             }
         }
@@ -29,8 +80,8 @@ public:
 
         TreeNode* root = new TreeNode(rootVal);
 
-        root->right = helper(inorder , postorder , valueIdx+1 , end , idx);
-        root->left = helper(inorder , postorder , start , valueIdx-1 , idx);
+        root->right = helper(inorder , postorder , i+1 , end , idx);
+        root->left = helper(inorder , postorder , start , i-1 , idx);
 
         return root;
     }
