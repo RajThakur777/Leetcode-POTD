e02c2dc92acad47acf83be8e3b3135ed626ce28e
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-02-25 14:10:42 +0530

    Time: 4 ms (62.15%), Space: 117.7 MB (21.75%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -25,6 +25,39 @@
 
 
 //Optimized Approach:
+// class Solution {
+// public:
+//     int MOD = 1e9 + 7;
+
+//     int numOfSubarrays(vector<int>& arr) {
+//         int n = arr.size();
+
+//         map<int , int> mpp;
+//         mpp[0] = 1;
+
+//         int sum = 0;
+//         int ans = 0;
+
+//         for(int i=0; i<n; i++){
+//             sum += arr[i];
+
+//             if(sum % 2 == 0){
+//                 ans = (ans + mpp[1]) % MOD;
+//             }
+//             else {
+//                 ans = (ans + mpp[0]) % MOD;
+//             }
+//             mpp[(sum % 2)]++;
+//         }
+//         return (ans % MOD);
+//     }
+// };
+
+
+
+
+
+//Using prefix sums:
 class Solution {
 public:
     int MOD = 1e9 + 7;
@@ -32,23 +65,27 @@ public:
     int numOfSubarrays(vector<int>& arr) {
         int n = arr.size();
 
-        map<int , int> mpp;
-        mpp[0] = 1;
+        vector<int> prefix(n);
+        prefix[0] = arr[0];
 
-        int sum = 0;
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
         int ans = 0;
+        int odd = 0;
+        int even = 1;
 
         for(int i=0; i<n; i++){
-            sum += arr[i];
-
-            if(sum % 2 == 0){
-                ans = (ans + mpp[1]) % MOD;
+            if(prefix[i] % 2 == 0){
+                ans = (ans + odd) % MOD;
+                even++;
             }
             else {
-                ans = (ans + mpp[0]) % MOD;
+                ans = (ans + even) % MOD;
+                odd++;
             }
-            mpp[(sum % 2)]++;
         }
-        return (ans % MOD);
+        return ans % MOD;
     }
 };
