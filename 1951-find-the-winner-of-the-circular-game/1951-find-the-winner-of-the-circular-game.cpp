b6fb6e8297aade02998ca4ea536f0216f63fec0b
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-01 09:39:53 +0530

    Time: 1 ms (54.61%), Space: 8.4 MB (45.56%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,19 +1,43 @@
+// class Solution {
+// public:
+//     int findTheWinner(int n, int k) { 
+//         queue<int> q;
+
+//         for(int i=1; i<=n; i++){
+//             q.push(i);
+//         } 
+
+//         while(q.size() > 1){
+//             for(int i=0; i<k-1; i++){
+//                 q.push(q.front());
+//                 q.pop();
+//             }
+//             q.pop();
+//         }
+//         return q.front();
+//     }
+// };
+
+
+
+//Using arrays:
 class Solution {
 public:
     int findTheWinner(int n, int k) { 
-        queue<int> q;
-
-        for(int i=1; i<=n; i++){
-            q.push(i);
-        } 
-
-        while(q.size() > 1){
-            for(int i=0; i<k-1; i++){
-                q.push(q.front());
-                q.pop();
-            }
-            q.pop();
+        vector<int> v;
+        for(int i=0; i<n; i++){
+            v.push_back(i+1);
         }
-        return q.front();
+
+        int i = 0;
+
+        while(v.size() > 1){
+            int idx = (i + k - 1) % v.size();
+
+            v.erase(v.begin() + idx);
+
+            i = idx;
+        }
+        return v.front();
     }
 };
