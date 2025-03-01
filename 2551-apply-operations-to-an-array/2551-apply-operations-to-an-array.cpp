cf3e46068f2a399fa2e3b86973b7cafc39324fd6
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-01 10:51:01 +0530

    Time: 0 ms (100%), Space: 13.2 MB (62.06%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -1,35 +1,67 @@
+// class Solution {
+// public:
+//     vector<int> applyOperations(vector<int>& nums) {
+//         int n = nums.size();
+
+//         for(int i=0; i<n-1; i++){
+//             if(nums[i] == nums[i+1]){
+//                 nums[i] *= 2;
+//                 nums[i+1] = 0;
+//             }
+//         }
+
+//         vector<int> zeroes;
+//         for(int i=0; i<n; i++){
+//             if(nums[i] == 0){
+//                 zeroes.push_back(nums[i]);
+//             }
+//         }
+
+//         vector<int> ans;
+//         for(int i=0; i<n; i++){
+//             if(nums[i] != 0){
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+
+//         int cnt = zeroes.size();
+
+//         while(cnt > 0){
+//             ans.push_back(0);
+//             cnt--;
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n = nums.size();
 
         for(int i=0; i<n-1; i++){
-            if(nums[i] == nums[i+1]){
+            if(nums[i] == nums[i+1] && nums[i] != 0){
                 nums[i] *= 2;
                 nums[i+1] = 0;
             }
         }
 
-        vector<int> zeroes;
-        for(int i=0; i<n; i++){
-            if(nums[i] == 0){
-                zeroes.push_back(nums[i]);
-            }
-        }
-
-        vector<int> ans;
+        int j = 0;
         for(int i=0; i<n; i++){
             if(nums[i] != 0){
-                ans.push_back(nums[i]);
+                nums[j] = nums[i];
+                j++;
             }
         }
 
-        int cnt = zeroes.size();
-
-        while(cnt > 0){
-            ans.push_back(0);
-            cnt--;
+        while(j < n){
+            nums[j] = 0;
+            j++;
         }
-        return ans;
+        return nums;
     }
 };
