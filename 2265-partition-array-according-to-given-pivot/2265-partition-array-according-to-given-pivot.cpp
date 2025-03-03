e64e60c9537783f4792178437301527723cdcf28
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-03 07:32:39 +0530

    Time: 7 ms (59.22%), Space: 133.6 MB (41.94%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,42 +1,9 @@
-// class Solution {
-// public:
-//     vector<int> pivotArray(vector<int>& nums, int pivot) {
-//         int n = nums.size();
-//         vector<int> ans(n , 0);
-
-//         int k = 0;
-
-//         for(int i=0; i<n; i++){
-//             if(nums[i] < pivot){
-//                 ans[k++] = nums[i];
-//             }
-//         }
-
-//         for(int i=0; i<n; i++){
-//             if(nums[i] == pivot){
-//                 ans[k++] = nums[i];
-//             }
-//         }
-
-//         for(int i=0; i<n; i++){
-//             if(nums[i] > pivot){
-//                 ans[k++] = nums[i];
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-
-
-
-
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int n = nums.size();
         vector<int> ans;
+
+        int n = nums.size();
 
         for(int i=0; i<n; i++){
             if(nums[i] < pivot){
