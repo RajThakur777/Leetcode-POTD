a6061db22719ffdf49dbfacb251890978165b8ab
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-03 08:07:07 +0530

    Time: 0 ms (100%), Space: 264 MB (52.23%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,61 +1,10 @@
-// class Solution {
-// public:
-//     bool doesValidArrayExist(vector<int>& derived) {   
-//         int n = derived.size();
-        
-//         int ones = 0;
-
-//         for(int i=0; i<n; i++){
-//             if(derived[i] == 1){
-//                 ones++;
-//             }
-//         }
-
-//         if(ones % 2 == 0){
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-
-
-
-
-
-// class Solution {
-// public:
-//     bool doesValidArrayExist(vector<int>& derived) {   
-//         int n = derived.size();
-        
-//         int xr = 0;
-
-//         for(int i=0; i<n; i++){
-//             xr ^= derived[i];
-//         }
-
-//         if(xr == 0){
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-
-
-
-
-
-
 class Solution {
 public:
-    bool doesValidArrayExist(vector<int>& derived) {   
-        int n = derived.size();
-        
-        int sum = 0;
-
-        for(int i=0; i<n; i++) sum += derived[i];
-
-        if(sum % 2 == 0) return true;
-
-        return false;
+    bool doesValidArrayExist(vector<int>& derived) {
+        int XOR = 0;
+        for (auto element : derived) {
+            XOR = XOR ^ element;
+        }
+        return XOR == 0;
     }
 };
