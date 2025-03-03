660f7a6a809e07d8334433c4456991697a931347
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-03 09:11:39 +0530

    Time: 9 ms (40.37%), Space: 127.7 MB (87.5%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,26 +1,62 @@
+// class Solution {
+// public:
+//     vector<int> pivotArray(vector<int>& nums, int pivot) {
+//         vector<int> ans;
+
+//         int n = nums.size();
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] < pivot){
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] == pivot){
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i] > pivot){
+//                 ans.push_back(nums[i]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int> ans;
-
         int n = nums.size();
 
-        for(int i=0; i<n; i++){
+        vector<int> ans(n);
+
+        int i = 0;
+        int j = n-1;
+
+        int i_ = 0;
+        int j_ = n-1;
+
+        while(i < n && j >= 0){
             if(nums[i] < pivot){
-                ans.push_back(nums[i]);
+                ans[i_] = nums[i];
+                i_++;
             }
+            if(nums[j] > pivot){
+                ans[j_] = nums[j];
+                j_--;
+            }
+            i++;
+            j--;
         }
-
-        for(int i=0; i<n; i++){
-            if(nums[i] == pivot){
-                ans.push_back(nums[i]);
-            }
-        }
-
-        for(int i=0; i<n; i++){
-            if(nums[i] > pivot){
-                ans.push_back(nums[i]);
-            }
+        while(i_ <= j_){
+            ans[i_] = pivot;
+            i_++;
         }
         return ans;
     }
