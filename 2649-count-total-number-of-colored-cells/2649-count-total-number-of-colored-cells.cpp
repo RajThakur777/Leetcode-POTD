05294f403dd6083ac92abe181eb36f94dd65d2a1
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-05 23:34:25 +0530

    Time: 2 ms (25.62%), Space: 7.8 MB (91.36%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,8 +1,25 @@
+// class Solution {
+// public:
+//     long long coloredCells(long long n) {
+//         long long val = (n * n) + (n -1) * (n - 1);
+
+//         return val;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
-    long long coloredCells(long long n) {
-        long long val = (n * n) + (n -1) * (n - 1);
-
-        return val;
+    long long coloredCells(int n) {
+        long long numBlueCells = 1;
+        int addend = 4;
+        while (--n) {
+            numBlueCells += addend;
+            addend += 4;
+        }
+        return numBlueCells;
     }
 };
