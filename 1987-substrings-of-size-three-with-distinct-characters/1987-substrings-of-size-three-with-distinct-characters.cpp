a27d8f75abd5650db52b7bd94e58ff8a56c537d5
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-07 21:37:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (56.3%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,15 +1,54 @@
+// class Solution {
+// public:
+//     int countGoodSubstrings(string s) {
+//         int n = s.size(); 
+
+//         int cnt = 0;
+
+//         if(s.size() < 3) return 0;  
+
+//         for(int i=0; i<=n-3; i++){
+//             if(s[i] != s[i+1] && s[i+1] != s[i+2] && s[i+2] != s[i]){
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+
+
+
+//Using Sliding Window:
 class Solution {
 public:
     int countGoodSubstrings(string s) {
-        int n = s.size(); 
+        int n = s.size();
+
+        int mp[26] = {0};
+
+        if(s.size() < 3) return 0;
 
         int cnt = 0;
+        int dup = 0;
 
-        if(s.size() < 3) return 0;  
+        for(int i=0; i<n; i++){
+            mp[s[i] - 'a']++;
 
-        for(int i=0; i<=n-3; i++){
-            if(s[i] != s[i+1] && s[i+1] != s[i+2] && s[i+2] != s[i]){
-                cnt++;
+            if(mp[s[i] - 'a'] == 2){
+                dup++;
+            }
+
+            if(i < 2) continue;
+
+            if(dup == 0) cnt++;
+
+            mp[s[i-2] - 'a']--;
+
+            if(mp[s[i-2] - 'a'] == 1){
+                dup--;
             }
         }
         return cnt;
