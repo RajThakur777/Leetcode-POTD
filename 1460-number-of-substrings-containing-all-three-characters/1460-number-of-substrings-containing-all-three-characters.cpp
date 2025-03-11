c494df7a1e2bbbd162f4203fde7c4d786cf3d43d
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-11 11:27:02 +0530

    Time: 0 ms (100%), Space: 10.9 MB (81.05%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,29 +1,59 @@
+// class Solution {
+// public:
+//     int numberOfSubstrings(string s) {
+//         int n = s.size();
+
+//         int ans = 0;
+
+//         int i = 0;
+//         map<char , int> mpp;
+
+//         for(int j=0; j<n; j++){
+//             mpp[s[j]]++;
+
+//             while(mpp.size() == 3){
+//                 ans += (n - j);
+
+//                 if(s[i] == 'a' || s[i] == 'b' || s[i] == 'c') {
+//                     mpp[s[i]]--;
+                    
+//                     if(mpp[s[i]] == 0){
+//                         mpp.erase(s[i]);
+//                     }
+//                 }
+//                 i++;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n = s.size();
+        int n = s.length();
 
-        int ans = 0;
+        int result = 0;
+        
+        vector<int> mp(3, 0); //0 - a, 1 - b, 2 - c
 
         int i = 0;
-        map<char , int> mpp;
+        int j = 0;
+        while(j < n) {
+            char ch = s[j];
+            mp[ch-'a']++;
 
-        for(int j=0; j<n; j++){
-            mpp[s[j]]++;
+            while(mp[0] > 0 && mp[1] > 0 && mp[2] > 0) {
+                result += (n - j);
 
-            while(mpp.size() == 3){
-                ans += (n - j);
-
-                if(s[i] == 'a' || s[i] == 'b' || s[i] == 'c') {
-                    mpp[s[i]]--;
-                    
-                    if(mpp[s[i]] == 0){
-                        mpp.erase(s[i]);
-                    }
-                }
+                mp[s[i] - 'a']--;
                 i++;
             }
+            j++;
         }
-        return ans;
+        return result;   
     }
 };
