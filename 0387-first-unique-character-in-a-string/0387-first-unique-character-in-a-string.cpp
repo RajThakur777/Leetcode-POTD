ad531fc40a5363e71df097368de144f9b7ecfa55
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-12 19:15:36 +0530

    Time: 11 ms (55.9%), Space: 14.7 MB (58.32%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,9 +1,31 @@
+// class Solution {
+// public:
+//     int firstUniqChar(string s) {
+//         int n = s.length();
+
+//         map<char , int> mpp;
+//         for(int i=0; i<n; i++){
+//             mpp[s[i]]++;
+//         }
+
+//         for(int i=0; i<n; i++){
+//             if(mpp[s[i]] == 1){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
     int firstUniqChar(string s) {
         int n = s.length();
 
-        map<char , int> mpp;
+        unordered_map<char , int> mpp;
         for(int i=0; i<n; i++){
             mpp[s[i]]++;
         }
