d08d87c38b7043124048fdad3b32fcfba0a046a7
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-14 17:18:52 +0530

    Time: 15 ms (25.36%), Space: 22.8 MB (74.92%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -5,7 +5,11 @@ public:
         long long cnt = 0;
 
         for(int i=0; i<piles.size(); i++){
-            cnt += (ceil(piles[i] * 1.0 / mid));
+            cnt += (piles[i] / mid);
+
+            if(piles[i] % mid != 0){
+                cnt++;
+            }
         }
         return cnt <= k;
     }
