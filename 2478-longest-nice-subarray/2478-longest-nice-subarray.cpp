4caa021cf42eae1fb4a67e3054beb1b381f0e53d
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-18 15:38:41 +0530

    Time: 0 ms (100%), Space: 60.9 MB (64.15%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -1,24 +1,56 @@
+// class Solution {
+// public:
+//     int longestNiceSubarray(vector<int>& nums) {   
+//         int n = nums.size();
+
+//         int i = 0;
+//         int res = 0;
+
+//         int ans = 1;
+
+//         for(int j=0; j<n; j++){
+//             while((res & nums[j]) != 0){
+//                 res ^= nums[i];
+//                 i++;
+//             }
+//             res |= nums[j];
+//             ans = max(ans , j - i + 1);
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {   
         int n = nums.size();
 
-        int i = 0;
-        int res = 0;
+        int maxLen = 1;
 
-        int ans = 1;
+        for(int i=0; i<n; i++){
+            int mask = 0;
+            for(int j=i; j<n; j++){
+                if((mask & nums[j]) != 0){
+                    break;
+                }
 
-        for(int j=0; j<n; j++){
-            while((res & nums[j]) != 0){
-                res ^= nums[i];
-                i++;
+                maxLen = max(maxLen , j - i + 1);
+                mask |= nums[j];
             }
-            res |= nums[j];
-            ans = max(ans , j - i + 1);
         }
-        return ans;
+        return maxLen;
     }
 };
+
+
+
+
 
 
 
