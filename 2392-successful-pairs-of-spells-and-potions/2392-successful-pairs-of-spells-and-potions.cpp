b5c776198a0d1e36b82d425e1591931d01dcc4ab
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-21 16:04:30 +0530

    Time: 41 ms (39.27%), Space: 128.2 MB (7.4%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,3 +1,27 @@
+// class Solution {
+// public:
+//     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+//         int n = spells.size();
+
+//         sort(potions.begin() , potions.end());
+
+//         vector<int> ans;
+
+//         for(int i=0; i<n; i++){
+//             long long val = spells[i];
+
+//             auto idx = lower_bound(potions.begin() , potions.end() , (success + val - 1) / val);
+
+//             ans.push_back(potions.end() - idx);
+//         }   
+//         return ans;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
@@ -8,11 +32,30 @@ public:
         vector<int> ans;
 
         for(int i=0; i<n; i++){
+            int low = 0;
+            int high = potions.size() - 1;
+
             long long val = spells[i];
 
-            auto idx = lower_bound(potions.begin() , potions.end() , (success + val - 1) / val);
+            int idx = -1;
+            while(low <= high){
+                int mid = (low + high) / 2;
 
-            ans.push_back(potions.end() - idx);
+                if((potions[mid]) * (val) >= success){
+                    idx = mid;
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            if(idx == -1) {
+                ans.push_back(0);
+            }
+            else {
+                ans.push_back(potions.size() - idx);
+            }
         }   
         return ans;
     }
