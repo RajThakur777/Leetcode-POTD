3c8b7bbe10e9b6865d6f064610a247add2c6e198
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-03-29 18:17:35 +0530

    Time: 260 ms (22.83%), Space: 62.3 MB (27.9%) - LeetHub

--- 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -1,3 +1,65 @@
+// //Brute force : BFS
+// class Solution {
+// public:
+//     int dx[4] = {-1 , 0 , 1 , 0};
+//     int dy[4] = {0 , 1 , 0 , -1};
+
+//     int bfs(vector<vector<int>> &grid , int m , int n , int val){
+//         vector<vector<bool>> visited(m , vector<bool>(n , false));
+
+//         queue<pair<int , int>> q;
+//         int points = 0;
+//         q.push({0 , 0});
+
+//         visited[0][0] = true;
+
+//         while(!q.empty()){
+//             pair<int , int> p = q.front();
+
+//             int x = p.first;
+//             int y = p.second;
+
+//             q.pop();
+
+//             if(grid[x][y] > val) continue;
+
+//             points++;
+
+//             for(int i=0; i<4; i++){
+//                 int nrow = x + dx[i];
+//                 int ncol = y + dy[i];
+
+//                 if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !visited[nrow][ncol] && grid[nrow][ncol] < val){
+//                     visited[nrow][ncol] = true;
+//                     q.push({nrow , ncol});
+//                 }
+//             }
+//         }
+//         return points;
+//     }
+
+//     vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+
+//         int q = queries.size();
+//         vector<int> res(q , 0);
+
+//         for(int i=0; i<q; i++){
+//             res[i] = bfs(grid , m , n , queries[i]);
+//         }
+//         return res;
+//     }
+// };
+
+
+
+
+
+
+
+
+
 class Solution {
 public:
     vector<vector<int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
