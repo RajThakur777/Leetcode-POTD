afffcea4c2c2d48340e0120e00ed40e02068416d
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-04-13 08:38:11 +0530

    Time: 0 ms (100%), Space: 45.4 MB (12.5%) - LeetHub

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/3846-minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/3846-minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -8,17 +8,6 @@ public:
             sum += nums[i];
         }
 
-        if(sum % k == 0) {
-            return 0;
-        }
-
-        int ans = 0;
-        for(int i=1; i<=1000000; i++){
-            if(i % k == 0 && i < sum){
-                ans = i;
-            }
-        }
-
-        return abs(ans-sum);
+        return (sum % k);
     }
 };
