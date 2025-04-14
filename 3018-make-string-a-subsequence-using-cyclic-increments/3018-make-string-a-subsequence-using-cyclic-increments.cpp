4eb70c12525fb4e878dc9af054b114de337a5a99
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-04-14 22:24:50 +0530

    Time: 0 ms (100%), Space: 17.1 MB (68.36%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,50 +1,25 @@
-// class Solution {
-// public:
-//     bool canMakeSubsequence(string str1, string str2) {
-//         int m = str1.size();
-//         int n = str2.size();
-
-//         int i = 0;
-//         int j = 0;
-
-//         while(i < m && j < n){
-//             if(str1[i] == str2[j]){
-//                 i++;
-//                 j++;
-//             }
-//             else if(str2[j] == (str1[i] - 'a' + 1) % 26 + 'a'){
-//                 i++;
-//                 j++;
-//             }
-//             else {
-//                 i++;
-//             }
-//         }
-
-//         if(j == n){
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-
-
-
-
-
-
-
 class Solution {
- public:
+public:
     bool canMakeSubsequence(string str1, string str2) {
+        int m = str1.size();
+        int n = str2.size();
+
+        int i = 0;
         int j = 0;
 
-        for(int i=0; i<str1.size(); i++){
-            if(((str2[j] - str1[i]) + 26)% 26 <= 1){
+        while(i < m && j < n){
+            if(str1[i] == str2[j]){
+                i++;
                 j++;
             }
-            if(j == str2.size()) return true;
+            else if(((str1[i] - 'a' + 1) % 26 + 'a') == str2[j]){
+                i++;
+                j++;
+            }
+            else {
+                i++;
+            }
         }
-        return false;
+        return (j == n);
     }
 };
