7dd8a8e9c3afe432a02f0441e84f97334e2f0bcf
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-04-24 09:27:58 +0530

    Time: 22 ms (54.35%), Space: 41.9 MB (49.93%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -1,4 +1,40 @@
 //Brute force:
+// class Solution {
+// public:
+//     int countCompleteSubarrays(vector<int>& nums) {
+//         int n = nums.size();
+
+//         set<int> st;
+//         for(int i=0; i<n; i++){
+//             st.insert(nums[i]);
+//         }
+
+//         int distinct = st.size();
+
+//         if(distinct == 1) {
+//             return (n * (n + 1)) / 2;
+//         }
+
+//         int cnt = 0;
+
+//         for(int i=0; i<n; i++){
+//             map<int , int> mpp;
+//             for(int j=i; j<n; j++) {
+//                 mpp[nums[j]]++;
+
+//                 if(mpp.size() == distinct){
+//                     cnt++;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+
+//Sliding Window + Hashing:
 class Solution {
 public:
     int countCompleteSubarrays(vector<int>& nums) {
@@ -17,14 +53,21 @@ public:
 
         int cnt = 0;
 
-        for(int i=0; i<n; i++){
-            map<int , int> mpp;
-            for(int j=i; j<n; j++) {
-                mpp[nums[j]]++;
+        map<int , int> mpp;
 
-                if(mpp.size() == distinct){
-                    cnt++;
+        int i = 0;
+        for(int j=0; j<n; j++){
+            mpp[nums[j]]++;
+
+            while(mpp.size() == distinct){
+                cnt += (n - j);
+
+                mpp[nums[i]]--;
+
+                if(mpp[nums[i]] == 0){
+                    mpp.erase(nums[i]);
                 }
+                i++;
             }
         }
         return cnt;
