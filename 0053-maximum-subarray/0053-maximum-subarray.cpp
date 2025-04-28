bd1956edf5ee1d41647091fcf74856bc0a0fa5e5
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-04-28 16:23:01 +0530

    Time: 0 ms (100%), Space: 71.7 MB (53.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,16 +3,17 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
 
-        int ms = 0;
+        int ms = INT_MIN;
         int cs = 0;
 
         for(int i=0; i<n; i++){
             cs += nums[i];
 
+            ms = max(ms , cs);
+            
             if(cs < 0){
                 cs = 0;
             }
-            ms = max(ms , cs);
         }
         return ms;
     }
