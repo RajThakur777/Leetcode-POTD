06d99554ce694f1cd4e77bdfca647f6b4aae95ea
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-05-20 13:14:31 +0530

    Time: 8 ms (61.63%), Space: 292.7 MB (96.48%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {    
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
 
-        vector<int> diff(n , 0);
+        vector<int> diff(n,  0);
 
         for(int i=0; i<queries.size(); i++){
             int l = queries[i][0];
@@ -14,7 +14,7 @@ public:
             if(r + 1 < n){
                 diff[r+1]--;
             }
-        }  
+        }
 
         for(int i=1; i<n; i++){
             diff[i] += diff[i-1];
