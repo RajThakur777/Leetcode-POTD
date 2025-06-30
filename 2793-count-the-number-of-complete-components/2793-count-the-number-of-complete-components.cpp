b7c9e2420d76904efce30ac3fe7abf25a181f1fd
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-06-30 16:25:22 +0530

    Time: 25 ms (89.53%), Space: 132.3 MB (58.33%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,39 +1,45 @@
+//DFS:
 class Solution {
 public:
 
-    void dfs(vector<vector<int>> &adj , vector<bool> &vis , int node , int &vertex , int &edges){
+    void dfs(int &vertex , int &edges , unordered_map<int , vector<int>> &mpp , vector<bool> &visited , int node) {
         vertex++;
-        vis[node] = true;
 
-        edges += adj[node].size();
+        visited[node] = true;
 
-        for(auto neigh : adj[node]){
-            if(vis[neigh] == false){
-                dfs(adj , vis , neigh , vertex , edges);
+        edges += mpp[node].size();
+
+        for(int &v : mpp[node]) {
+            if(!visited[v]) {
+                dfs(vertex , edges , mpp , visited , v);
             }
         }
     }
 
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<bool> vis(n , false);
+    int countCompleteComponents(int n, vector<vector<int>>& edges) {  
+        unordered_map<int , vector<int>> mpp;
 
-        vector<vector<int>> adj(n);
-        for(auto it:edges){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+        for(int i=0; i<edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            mpp[u].push_back(v);
+            mpp[v].push_back(u);
         }
 
         int ans = 0;
 
-        for(int i=0; i<n; i++){
-            if(vis[i] == false){
-                int v=  0;
-                int e = 0;
-                
-                dfs(adj , vis , i , v , e);
+        vector<bool> visited(n , false);
 
-                if((v * (v - 1)) == e){
-                   ans++;
+        for(int i=0; i<n; i++) {
+            if(!visited[i]) {
+                int v = 0;
+                int e = 0;
+
+                dfs(v , e , mpp , visited , i);
+
+                if((v * (v - 1)) == e) {
+                    ans++;
                 }
             }
         }
