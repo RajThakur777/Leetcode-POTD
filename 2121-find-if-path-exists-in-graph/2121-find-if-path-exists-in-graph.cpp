a803fcc94f95511160f15f431705569e3ef3b827
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-06-30 15:23:52 +0530

    Time: 457 ms (34.75%), Space: 328.9 MB (39.66%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,24 +1,51 @@
 //DFS:
+// class Solution {
+// public:
+
+//     bool dfs(int node , int destination , unordered_map<int , vector<int>> &mpp , vector<bool> &visited) {
+//         visited[node] = true;
+
+//         if(node == destination) return true;
+
+//         for(int &v : mpp[node]) {
+//             if(!visited[v]) {
+//                 if(dfs(v , destination , mpp , visited)) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+
+//     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+//         unordered_map<int , vector<int>> mpp;
+
+//         for(int i=0; i<edges.size(); i++) {
+//             int u = edges[i][0];
+//             int v = edges[i][1];
+
+//             mpp[u].push_back(v);
+//             mpp[v].push_back(u);
+//         }
+
+//         vector<bool> visited(n , false);
+
+//         return dfs(source , destination , mpp , visited);
+//     }
+// };
+
+
+
+
+
+
+//BFS:
 class Solution {
 public:
-
-    bool dfs(int node , int destination , unordered_map<int , vector<int>> &mpp , vector<bool> &visited) {
-        visited[node] = true;
-
-        if(node == destination) return true;
-
-        for(int &v : mpp[node]) {
-            if(!visited[v]) {
-                if(dfs(v , destination , mpp , visited)) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
         unordered_map<int , vector<int>> mpp;
+
+        if(source == destination) return true;
 
         for(int i=0; i<edges.size(); i++) {
             int u = edges[i][0];
@@ -30,6 +57,23 @@ public:
 
         vector<bool> visited(n , false);
 
-        return dfs(source , destination , mpp , visited);
+        queue<int> q;
+        q.push(source);
+        visited[source] = true;
+
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+
+            if(curr == destination) return true;
+
+            for(int &v : mpp[curr]) {
+                if(!visited[v]) {
+                    visited[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+        return false;
     }
 };
