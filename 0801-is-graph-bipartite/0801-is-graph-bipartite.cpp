ed878d4f4486ef24f11cbe93c07401ee8eb2ceea
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-07-01 15:02:08 +0530

    Time: 0 ms (100%), Space: 17.6 MB (25.46%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,20 +1,70 @@
-//DFS:
+// //DFS:
+// class Solution {
+// public:
+
+//     bool DFS(vector<vector<int>>& graph , int node , vector<int> &color , int currColor) {
+//         color[node] = currColor;
+
+//         for(int &v : graph[node]) {
+//             if(color[v] == color[node]) {
+//                 return false;
+//             }
+
+//             if(color[v] == -1) {
+//                 color[v] = 1 - color[node];
+
+//                 if(DFS(graph , v , color , color[v]) == false) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+
+//     bool isBipartite(vector<vector<int>>& graph) {
+//         int n = graph.size();
+
+//         vector<int> color(n , -1);
+
+//         for(int i=0; i<n; i++) {
+//             if(color[i] == -1) {
+//                 if(DFS(graph , i , color , 1) == false) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+
+
+
+
+
+//BFS:
 class Solution {
 public:
 
-    bool DFS(vector<vector<int>>& graph , int node , vector<int> &color , int currColor) {
+    bool BFS(vector<vector<int>>& graph , int node , vector<int> &color , int currColor) {
+        queue<int> q;
+        q.push(node);
+
         color[node] = currColor;
 
-        for(int &v : graph[node]) {
-            if(color[v] == color[node]) {
-                return false;
-            }
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
 
-            if(color[v] == -1) {
-                color[v] = 1 - color[node];
-
-                if(DFS(graph , v , color , color[v]) == false) {
+            for(int &v : graph[curr]) {
+                if(color[v] == color[curr]) {
                     return false;
+                }
+
+                if(color[v] == -1) {
+                    color[v] = 1 - color[curr];
+                    q.push(v);
                 }
             }
         }
@@ -28,7 +78,7 @@ public:
 
         for(int i=0; i<n; i++) {
             if(color[i] == -1) {
-                if(DFS(graph , i , color , 1) == false) {
+                if(BFS(graph , i , color , 1) == false) {
                     return false;
                 }
             }
