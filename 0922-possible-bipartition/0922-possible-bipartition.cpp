f3ae47be16f270d972c2bfa1de6fed1aaa485ea7
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-07-01 15:04:40 +0530

    Time: 37 ms (63.04%), Space: 71.6 MB (46.64%) - LeetHub

--- 0922-possible-bipartition/0922-possible-bipartition.cpp
+++ 0922-possible-bipartition/0922-possible-bipartition.cpp
@@ -1,20 +1,77 @@
-//DFS:
+// //DFS:
+// class Solution {
+// public:
+
+//     bool DFS(unordered_map<int , vector<int>> &mpp , int node , vector<int> &color , int currentColor) {
+//         color[node] = currentColor;
+
+//         for(int &v : mpp[node]) {
+//             if(color[v] == color[node]) {
+//                 return false;
+//             }
+
+//             if(color[v] == -1) {
+//                 color[v] = 1 - color[node];
+
+//                 if(DFS(mpp , v , color , color[v]) == false) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+
+//     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+//         unordered_map<int , vector<int>> mpp;
+
+//         int N = dislikes.size();
+
+//         for(int i=0; i<N; i++){
+//             int u = dislikes[i][0];
+//             int v = dislikes[i][1];
+
+//             mpp[u].push_back(v);
+//             mpp[v].push_back(u);
+//         }
+
+//         vector<int> color(n+1 , -1);
+
+//         for(int i=1; i<=n; i++) {
+//             if(color[i] == -1) {
+//                 if(DFS(mpp , i , color , 1) == false) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
+
+
+
+
+
+
+//BFS:
 class Solution {
 public:
 
-    bool DFS(unordered_map<int , vector<int>> &mpp , int node , vector<int> &color , int currentColor) {
-        color[node] = currentColor;
+    bool BFS(unordered_map<int , vector<int>> &mpp , int node , vector<int> &color , int currentColor) {
+        queue<int> q;
+        q.push(node);
 
-        for(int &v : mpp[node]) {
-            if(color[v] == color[node]) {
-                return false;
-            }
+        color[node]= currentColor;
 
-            if(color[v] == -1) {
-                color[v] = 1 - color[node];
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
 
-                if(DFS(mpp , v , color , color[v]) == false) {
-                    return false;
+            for(int &v : mpp[curr]) {
+                if(color[v] == color[curr]) return false;
+
+                if(color[v] == -1) {
+                    color[v] = 1 - color[curr];
+                    q.push(v);
                 }
             }
         }
@@ -38,7 +95,7 @@ public:
 
         for(int i=1; i<=n; i++) {
             if(color[i] == -1) {
-                if(DFS(mpp , i , color , 1) == false) {
+                if(BFS(mpp , i , color , 1) == false) {
                     return false;
                 }
             }
