e1913505e86bf19cde94460cc2128bdb91955a9a
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-07-06 11:36:47 +0530

    Time: 87 ms (65.55%), Space: 82 MB (70.15%) - LeetHub

--- 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
+++ 1995-finding-pairs-with-a-certain-sum/1995-finding-pairs-with-a-certain-sum.cpp
@@ -1,32 +1,32 @@
 class FindSumPairs {
 public:
-    map<int , int> mpp;
-    vector<int> v1 , v2;
+    vector<int> arr1 , arr2;
+    unordered_map<int , int> mpp;
 
     FindSumPairs(vector<int>& nums1, vector<int>& nums2) {
-        v1 = nums1;
-        v2 = nums2;
+        arr1 = nums1;
+        arr2 = nums2;
 
-        sort(v1.begin() , v1.end());
-
-        for(int i=0; i<v2.size(); i++){
-            mpp[v2[i]]++;
+        for(int i=0; i<arr2.size(); i++) {
+            mpp[arr2[i]]++;
         }
     }
     
-    void add(int index, int val) {
-        int ans = v2[index];
-        mpp[ans]--;
-        v2[index] += val;
-        mpp[v2[index]]++;
+    void add(int index, int val) { 
+        int v = arr2[index];
+        mpp[v]--;
+        arr2[index] += val;
+        mpp[arr2[index]]++;
     }
     
     int count(int tot) {
         int cnt = 0;
 
-        for(int i=0; i<v1.size(); i++){
-            if(mpp.find(tot - v1[i]) != mpp.end()){
-                cnt += mpp[tot - v1[i]];
+        for(int i=0; i<arr1.size(); i++) {
+            int val = tot - arr1[i];
+
+            if(mpp.find(val) != mpp.end()) {
+                cnt += mpp[val];
             }
         }
         return cnt;
