7d1357bb86830f5805b7ff9e904a0bf7f22ec7bf
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-07-14 17:22:41 +0530

    Time: 61 ms (15.13%), Space: 66.3 MB (21.23%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,37 +1,45 @@
+//Topological Sorting Concept:
 class Solution {
-    vector<bool> safe_nodes;
-    bool checkSafeState(vector<vector<int>>& graph,vector<int>& visited,int curr){
-        visited[curr]=1;//Mark Visited
-        bool is_safe=true;
-
-        for(int nbr: graph[curr]){
-            if(visited[nbr]==0){//Unvisited
-                is_safe &= checkSafeState(graph,visited,nbr);
-            }else if(visited[nbr]==1){//Cycle
-                return safe_nodes[curr]=false;
-            }else if(visited[nbr]==2){//Processed
-                is_safe &= safe_nodes[nbr];
-            }
-        }
-        visited[curr]=2;//Mark Processed
-        return safe_nodes[curr]=is_safe;
-    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n=graph.size();
-        safe_nodes = vector<bool>(n,true);//Everyone is safe initially
+        vector<int> ans;
 
-        vector<int> visited(n,0);//0->Unvisited...1->Visited...2->Processed
-        for(int i=0;i<n;++i){
-            if(visited[i]==0){
-                checkSafeState(graph,visited,i);
+        int n = graph.size();
+
+        vector<vector<int>> adj(n);
+
+        vector<int> indegree(n);
+
+        for(int i=0; i<n; i++) {
+            for(auto &v : graph[i]) {
+                adj[v].push_back(i);
+                indegree[i]++;
             }
         }
-        vector<int> res;
-        for(int i=0;i<n;++i)
-            if(safe_nodes[i]==true)
-                res.push_back(i);
 
-        return res;
+        queue<int> q;
+        for(int i=0; i<n; i++) {
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for(auto &it : adj[node]) {
+                indegree[it]--;
+
+                if(indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+        sort(ans.begin() , ans.end());
+
+        return ans;
     }
 };
