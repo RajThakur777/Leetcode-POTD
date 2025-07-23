8629ae0917f9ba68d1e5261809e59e8d760c1995
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-07-23 16:47:02 +0530

    Time: 74 ms (72.9%), Space: 113 MB (58.65%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.cpp
@@ -1,48 +1,52 @@
 class Solution {
 public:
-    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) { 
-        int n = isWater.size();
-        int m = isWater[0].size();
+    int m;
+    int n;
 
-        vector<vector<int>> vis(n , vector<int> (m , 0));
-        vector<vector<int>> dist(n , vector<int> (m , 0));
+    vector<vector<int>> directions = {{-1 , 0} , {1 , 0} , {0 , -1} , {0 , 1}};
 
-        queue<pair<pair<int , int> , int>> q;
+    bool is_Safe(int x , int y) {
+        return (x >= 0 && x < m && y >= 0 && y < n);
+    }
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(isWater[i][j] == 1){
-                    q.push({{i , j} , 0});
-                    vis[i][j] = 1;
-                }
-                else {
-                    vis[i][j] = 0;
-                }
-            }
-        }     
+    vector<vector<int>> highestPeak(vector<vector<int>>& isWater) {
+        m = isWater.size();
+        n = isWater[0].size();
 
-        while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
 
-            int steps = q.front().second;
-            q.pop();
+        vector<vector<int>> ans(m , vector<int>(n , -1));
 
-            dist[row][col] = steps;
+        queue<pair<int , int>> q;
 
-            int dx[4] = {-1 , 0 , 1 , 0};
-            int dy[4] = {0 , 1 , 0 , -1};
-
-            for(int i=0; i<4; i++){
-                int nrow = row + dx[i];
-                int ncol = col + dy[i];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0){
-                    vis[nrow][ncol] = 1;
-                    q.push({{nrow , ncol} , steps + 1});
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(isWater[i][j] == 1) {
+                    ans[i][j] = 0;
+                    q.push({i , j});
                 }
             }
         }
-        return dist;
+
+        while(!q.empty()) {
+            int sz = q.size();
+
+            while(sz--) {
+                int x = q.front().first;
+                int y = q.front().second;
+
+                q.pop();
+
+                for(auto &dir : directions) {
+                    int x_ = x + dir[0];
+                    int y_ = y + dir[1];
+
+                    if(is_Safe(x_ , y_) && ans[x_][y_] == -1) {
+                        ans[x_][y_] = 1 + ans[x][y];
+                        q.push({x_ , y_});
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
