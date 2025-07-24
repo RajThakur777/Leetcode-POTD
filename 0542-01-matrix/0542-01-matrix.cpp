d536fa5b003984b22240463816a63b5c6da06513
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-07-24 08:29:39 +0530

    Time: 20 ms (54.46%), Space: 34.6 MB (93.88%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,46 +1,56 @@
+//Multi-Source BFS:
 class Solution {
 public:
+
+    int m;
+    int n;
+
+    vector<vector<int>> directions = {{-1 , 0} , {1 , 0} , {0 , -1} , {0 , 1}};
+
+    bool is_Safe(int x , int y , vector<vector<int>> &mat) {
+        m = mat.size();
+        n = mat[0].size();
+
+        return (x >= 0 && x < m && y >= 0 && y < n);
+    }
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
+        m = mat.size();
+        n = mat[0].size();
 
-        vector<vector<int>> vis(n , vector<int>(m , 0));
-        vector<vector<int>> dist(n , vector<int>(m , 0));
+        vector<vector<int>> ans(m , vector<int>(n , -1));
 
-        queue<pair<pair<int , int> , int>> q;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(mat[i][j] == 0){
-                    q.push({{i , j} , 0});
-                    vis[i][j] = 1;
-                }
-                else {
-                    vis[i][j] = 0;
-                }
-            }
-        } 
-        while(!q.empty()){
-            int row = q.front().first.first;
-            int col = q.front().first.second;
+        queue<pair<int , int>> q;
 
-            int steps = q.front().second;
-            q.pop();
-
-            dist[row][col] = steps;
-
-            int dx[4] = {-1 , 0 , +1 , 0};
-            int dy[4] = {0 , +1 , 0 , -1};
-
-            for(int i=0; i<4; i++){
-                int nrow = row + dx[i];
-                int ncol = col + dy[i];
-
-                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0){
-                   vis[nrow][ncol] = 1;
-                   q.push({{nrow , ncol} , steps + 1});   
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(mat[i][j] == 0) {
+                    ans[i][j] = 0;
+                    q.push({i , j});
                 }
             }
         }
-        return dist;
+
+        while(!q.empty()) {
+            int sz = q.size();
+
+            while(sz--) {
+                int x = q.front().first;
+                int y = q.front().second;
+
+                q.pop();
+
+                for(auto &dir : directions) {
+                    int x_ = x + dir[0];
+                    int y_ = y + dir[1];
+
+                    if(is_Safe(x_ , y_ , mat) && ans[x_][y_] == -1) {
+                        ans[x_][y_] = 1 + ans[x][y];
+                        q.push({x_ , y_});
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
