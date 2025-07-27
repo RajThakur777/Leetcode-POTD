4c3c572d3d65d8a6fcb02081ee699a49a9eba51d
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-07-27 16:22:04 +0530

    Time: 29 ms (77.8%), Space: 97.9 MB (54.84%) - LeetHub

--- 2035-count-sub-islands/2035-count-sub-islands.cpp
+++ 2035-count-sub-islands/2035-count-sub-islands.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
+    int m;
+    int n;
 
-    //DFS
-    bool checkSubIsland(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j) {
-        if(i < 0 || i >= grid1.size() || j < 0 || j >= grid1[0].size()) {
-            return true;
-        }
-        
-        if(grid2[i][j] != 1) { 
-            return true;
-        }
+    bool dfs(int i , int j , vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid2[i][j] == 0) return true;
 
-        grid2[i][j] = -1; 
+        grid2[i][j] = 0;
 
-        bool result = (grid1[i][j] == 1); 
+        bool res = (grid1[i][j] == 1);
 
-        result = result & checkSubIsland(grid1, grid2, i+1, j); //Down
-        result = result & checkSubIsland(grid1, grid2, i-1, j); //Up
-        result = result & checkSubIsland(grid1, grid2, i, j+1);  //Right
-        result = result & checkSubIsland(grid1, grid2, i, j-1); //Left
+        res = res & dfs(i-1 , j , grid1 , grid2);
+        res = res & dfs(i+1 , j , grid1 , grid2);
+        res = res & dfs(i , j-1 , grid1 , grid2);
+        res = res & dfs(i , j+1 , grid1 , grid2);
 
-        return result;
-
+        return res;
     }
 
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
-        int subIslands = 0;
-        int m = grid2.size(); 
-        int n = grid2[0].size(); 
+        m = grid1.size();
+        n = grid1[0].size();
 
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(grid2[i][j] == 1 && checkSubIsland(grid1, grid2, i, j)) {
-                    subIslands++;
+        int cnt = 0;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid2[i][j] == 1 && dfs(i , j , grid1 , grid2)) {
+                    cnt++;
                 }
             }
         }
-        return subIslands;
+        return cnt;
     }
 };
