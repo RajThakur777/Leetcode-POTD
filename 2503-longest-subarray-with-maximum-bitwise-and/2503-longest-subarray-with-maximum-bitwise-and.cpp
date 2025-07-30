cdd8dc25d2005ee24b95f2d9795448ca9e3aa0a2
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-07-30 15:08:18 +0530

    Time: 3 ms (42.01%), Space: 86.2 MB (38.1%) - LeetHub

--- 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/2503-longest-subarray-with-maximum-bitwise-and.cpp
@@ -5,19 +5,23 @@ public:
 
         int maxi = *max_element(nums.begin() , nums.end());
 
-        int cnt = 0;
         int ans = INT_MIN;
-
-        for(int i=0; i<n; i++){
-            if(nums[i] == maxi){
+        
+        int cnt = 0;
+        for(int i=0; i<n; i++) {
+            if(nums[i] == maxi) {
                 cnt++;
             }
             else {
-                ans = max(ans , cnt);
+                if(cnt > 0) {
+                    ans = max(ans , cnt);
+                }
                 cnt = 0;
             }
         }
-        ans = max(ans , cnt);
+        if(cnt > 0) {
+            ans = max(ans , cnt);
+        }
         return ans;
     }
 };
