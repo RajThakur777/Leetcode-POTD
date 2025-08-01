6a7d5ed06c3705087de709bf680e0dfd48108c75
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-01 21:39:29 +0530

    Time: 275 ms (24.33%), Space: 199 MB (13.93%) - LeetHub

--- 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
+++ 2445-reachable-nodes-with-restrictions/2445-reachable-nodes-with-restrictions.cpp
@@ -1,69 +1,116 @@
-//DSU:
+// //DSU:
+// class Solution {
+// public:
+
+//     vector<int> parent;
+//     vector<int> rank;
+
+//     int find(int x) {
+//         if(x == parent[x]) return x;
+
+//         return parent[x] = find(parent[x]);
+//     }
+
+//     void Union(int x , int y) {
+//         int parent_x = find(x);
+//         int parent_y = find(y);
+
+//         if(parent_x == parent_y) return;
+
+//         if(rank[parent_x] > rank[parent_y]) {
+//             parent[parent_y] = parent_x; 
+//         }
+//         else if(rank[parent_y] > rank[parent_x]) {
+//             parent[parent_x] = parent_y;
+//         }
+//         else {
+//             parent[parent_y] = parent_x;
+//             rank[parent_x]++;
+//         }
+//     }
+
+//     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) { 
+//         parent.resize(n);
+//         rank.resize(n , 0);
+
+//         for(int i=0; i<n; i++) {
+//             parent[i] = i;
+//         }  
+
+//         set<int> st;
+//         for(int i=0; i<restricted.size(); i++) {
+//             st.insert(restricted[i]);
+//         }
+
+//         int cnt = 0;
+
+//         for(int i=0; i<edges.size(); i++) {
+//             int u = edges[i][0];
+//             int v = edges[i][1];
+
+//             int parent_u = find(u);
+//             int parent_v = find(v);
+
+//             if((st.find(u) != st.end()) || (st.find(v) != st.end()) || (parent_u == parent_v)) {
+                
+//                 continue;
+//             }
+
+//             Union(u , v);
+//         }
+
+//         for(int i=0; i<n; i++) {
+//             if(st.find(i) == st.end() && find(i) == find(0)) {
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+
+
+
+//DFS:
 class Solution {
 public:
 
-    vector<int> parent;
-    vector<int> rank;
+    int dfs(int node , unordered_map<int , vector<int>> &mpp , vector<bool> &visited , set<int> &st) {
+        int cnt = 0;
 
-    int find(int x) {
-        if(x == parent[x]) return x;
+        visited[node] = true;
+        cnt++;
 
-        return parent[x] = find(parent[x]);
-    }
-
-    void Union(int x , int y) {
-        int parent_x = find(x);
-        int parent_y = find(y);
-
-        if(parent_x == parent_y) return;
-
-        if(rank[parent_x] > rank[parent_y]) {
-            parent[parent_y] = parent_x; 
+        for(auto it : mpp[node]) {
+            if(!visited[it] && st.find(it) == st.end()) {
+                cnt += dfs(it , mpp , visited , st);
+            }
         }
-        else if(rank[parent_y] > rank[parent_x]) {
-            parent[parent_x] = parent_y;
-        }
-        else {
-            parent[parent_y] = parent_x;
-            rank[parent_x]++;
-        }
+        return cnt;
     }
 
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) { 
-        parent.resize(n);
-        rank.resize(n , 0);
+        int N = edges.size();
 
-        for(int i=0; i<n; i++) {
-            parent[i] = i;
-        }  
+        unordered_map<int , vector<int>> mpp;
+
+        for(int i=0; i<N; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            mpp[u].push_back(v);
+            mpp[v].push_back(u);
+        }
+
+        vector<bool> visited(n , false);
 
         set<int> st;
         for(int i=0; i<restricted.size(); i++) {
             st.insert(restricted[i]);
         }
 
-        int cnt = 0;
-
-        for(int i=0; i<edges.size(); i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-
-            int parent_u = find(u);
-            int parent_v = find(v);
-
-            if((st.find(u) != st.end()) || (st.find(v) != st.end()) || (parent_u == parent_v)) {
-                
-                continue;
-            }
-
-            Union(u , v);
-        }
-
-        for(int i=0; i<n; i++) {
-            if(st.find(i) == st.end() && find(i) == find(0)) {
-                cnt++;
-            }
-        }
-        return cnt;
+        return dfs(0 , mpp , visited , st);
     }
 };
