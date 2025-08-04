0df773eb01ed8ef03365edde069be446130c5a30
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-04 08:45:11 +0530

    Time: 47 ms (33.19%), Space: 79.1 MB (8.05%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -3,22 +3,24 @@ public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
 
-        int ans = INT_MIN;
-
         map<int , int> mpp;
+
+        int ans = INT_MIN;
         int i = 0;
 
-        for(int j=0; j<n; j++){
+        for(int j=0; j<n; j++) {
             mpp[fruits[j]]++;
 
-            while(mpp.size() > 2){
+            while(mpp.size() > 2) {
                 mpp[fruits[i]]--;
 
-                if(mpp[fruits[i]] == 0) mpp.erase(fruits[i]);
+                if(mpp[fruits[i]] == 0) {
+                    mpp.erase(fruits[i]);
+                }
                 i++;
             }
 
-            ans = max(ans , j - i + 1);
+            ans = max(ans , (j - i + 1));
         }
         return ans;
     }
