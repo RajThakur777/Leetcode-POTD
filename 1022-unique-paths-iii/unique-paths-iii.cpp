157e79172e803eec2488fc297eedc4684b7ed46d
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-10 16:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (99.14%) - LeetSync

---
+++ 1022-unique-paths-iii/unique-paths-iii.cpp
@@ -0,0 +1,53 @@
+//Code-1:
+class Solution {
+public:
+    int m;
+    int n;
+
+    int dfs(int i , int j , int count , vector<vector<int>> &grid) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) return 0;
+
+        if(grid[i][j] == 2) {
+            return (count == 0 ? 1 : 0);
+        }
+
+        int temp = grid[i][j];
+        grid[i][j] = -1;
+        count--;
+
+        int paths = 0;
+        paths += dfs(i-1 , j , count , grid);
+        paths += dfs(i+1 , j , count , grid);
+        paths += dfs(i , j-1 , count , grid);
+        paths += dfs(i , j+1 , count , grid);
+
+        grid[i][j] = temp;
+        count++;
+
+        return paths;
+    }
+
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+
+        int start_x = 0;
+        int start_y = 0;
+
+        int empty = 0;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j] == 1) {
+                    start_x = i;
+                    start_y = j;
+                }
+                else if(grid[i][j] == 0) {
+                    empty++;
+                }
+            }
+        }
+
+        return dfs(start_x , start_y , empty+1 , grid);
+    }
+};
