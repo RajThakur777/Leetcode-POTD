5400d58c80355e8064c847e8a55894f500641b6f
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-10 16:30:04 +0530

    Time: 0 ms (100%), Space: 9.3 MB (75.34%) - LeetHub

--- 1022-unique-paths-iii/1022-unique-paths-iii.cpp
+++ 1022-unique-paths-iii/1022-unique-paths-iii.cpp
@@ -1,30 +1,88 @@
-//Code-1:
+// //Code-1:
+// class Solution {
+// public:
+//     int m;
+//     int n;
+
+//     int dfs(int i , int j , int count , vector<vector<int>> &grid) {
+//         if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) return 0;
+
+//         if(grid[i][j] == 2) {
+//             return (count == 0 ? 1 : 0);
+//         }
+
+//         int temp = grid[i][j];
+//         grid[i][j] = -1;
+//         count--;
+
+//         int paths = 0;
+//         paths += dfs(i-1 , j , count , grid);
+//         paths += dfs(i+1 , j , count , grid);
+//         paths += dfs(i , j-1 , count , grid);
+//         paths += dfs(i , j+1 , count , grid);
+
+//         grid[i][j] = temp;
+//         count++;
+
+//         return paths;
+//     }
+
+//     int uniquePathsIII(vector<vector<int>>& grid) {
+//         m = grid.size();
+//         n = grid[0].size();
+
+//         int start_x = 0;
+//         int start_y = 0;
+
+//         int empty = 0;
+
+//         for(int i=0; i<m; i++) {
+//             for(int j=0; j<n; j++) {
+//                 if(grid[i][j] == 1) {
+//                     start_x = i;
+//                     start_y = j;
+//                 }
+//                 else if(grid[i][j] == 0) {
+//                     empty++;
+//                 }
+//             }
+//         }
+
+//         return dfs(start_x , start_y , empty+1 , grid);
+//     }
+// };
+
+
+
+
+
+//Code-2:
 class Solution {
 public:
     int m;
     int n;
 
-    int dfs(int i , int j , int count , vector<vector<int>> &grid) {
-        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) return 0;
+    void dfs(int i , int j , int count , vector<vector<int>> &grid , int &res) {
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == -1) return;
 
         if(grid[i][j] == 2) {
-            return (count == 0 ? 1 : 0);
+            if(count == 0) {
+                res++;
+            }
+            return;
         }
 
         int temp = grid[i][j];
         grid[i][j] = -1;
         count--;
 
-        int paths = 0;
-        paths += dfs(i-1 , j , count , grid);
-        paths += dfs(i+1 , j , count , grid);
-        paths += dfs(i , j-1 , count , grid);
-        paths += dfs(i , j+1 , count , grid);
+        dfs(i-1 , j , count , grid , res);
+        dfs(i+1 , j , count , grid , res);
+        dfs(i , j-1 , count , grid , res);
+        dfs(i , j+1 , count , grid , res);
 
         grid[i][j] = temp;
         count++;
-
-        return paths;
     }
 
     int uniquePathsIII(vector<vector<int>>& grid) {
@@ -48,6 +106,10 @@ public:
             }
         }
 
-        return dfs(start_x , start_y , empty+1 , grid);
+        int res = 0;
+
+        dfs(start_x , start_y , empty+1 , grid , res);
+
+        return res;
     }
 };
