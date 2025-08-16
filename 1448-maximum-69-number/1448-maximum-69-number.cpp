22a02d98a07a4472b806d3fba362b0911674a241
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-16 07:53:19 +0530

    Time: 0 ms (100%), Space: 8.1 MB (41.82%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        int ans;
-        string num_str = to_string(num);
-        for(int i=0;i<num_str.size();i++){
-            if(num_str[i]=='6'){
-                num_str[i]='9';
+        string res = to_string(num);
+
+        for(int i=0; i<res.size(); i++) {
+            if(res[i] == '6') {
+                res[i] = '9';
                 break;
             }
-        }
-        return stoi(num_str);
+        } 
+        return stoi(res);
     }
 };
