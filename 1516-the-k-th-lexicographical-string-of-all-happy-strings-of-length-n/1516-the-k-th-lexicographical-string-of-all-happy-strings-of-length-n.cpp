888c0f5f7a5a40bd6cd29daa3e0db5561cf99645
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-20 10:28:14 +0530

    Time: 7 ms (57.41%), Space: 24.5 MB (41.71%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,118 +1,29 @@
-// //Recursion:
-// class Solution {
-// public:
-
-//     void helper(int idx , int n , vector<string> &ans , string curr , int k){
-//         if(idx == n){
-//             ans.push_back(curr);
-//             return;
-//         }
-
-//         for(char ch='a'; ch<='c'; ch++){
-//             if(curr.size() > 0 && curr.back() == ch) continue;
-
-//             helper(idx+1 , n , ans , curr+ch , k);
-//         }
-//     }
-
-//     string getHappyString(int n, int k) {
-//         vector<string> ans;
-
-//         helper(0 , n , ans , "" , k);
-
-//         sort(ans.begin() , ans.end());
-
-//         if(ans.size() < k){
-//             return "";
-//         }
-//         return ans[k-1];
-//     }
-// };
-
-
-
-
-
-
-//Recursion + Backtracking:
-// class Solution {
-// public:
-
-//     void helper(string curr , vector<string> &ans , int n){
-//         if(curr.size() == n){
-//             ans.push_back(curr);
-//             return;
-//         }
-
-//         //try out every possibilities:
-//         for(char ch='a'; ch<='c'; ch++){
-//             if(!curr.empty() && curr.back() == ch) continue;
-
-//             //DO
-//             curr.push_back(ch);
-
-//             //EXPLORE
-//             helper(curr , ans , n);
-
-//             //UNDO
-//             curr.pop_back();
-//         }
-//     }
-
-//     string getHappyString(int n, int k) {
-//         vector<string> ans;
-
-//         helper("" , ans , n);
-
-//         if(ans.size() < k){
-//             return "";
-//         }
-//         return ans[k-1];
-//     }
-// };
-
-
-
-
-
 class Solution {
 public:
-    void solve(int n, string &curr, int &count, int k, string &result) {
-        if (curr.length() == n) {
-            count++;
-            if (count == k) {
-                result = curr;  // Store only the k-th string
-            }
+    void solve(int idx , int n , int k , vector<string> &res , string &temp) {
+        if(idx == n) {
+            res.push_back(temp);
             return;
         }
-        
-        for (char ch = 'a'; ch <= 'c'; ch++) {
-            if (!curr.empty() && curr.back() == ch)
-                continue;
-            
-            // Do
-            curr.push_back(ch);
 
-            // Explore
-            solve(n, curr, count, k, result);
-
-            // If result is found, exit early
-            if (!result.empty()) return;
-
-            // Undo
-            curr.pop_back();
+        for(char ch='a'; ch<='c'; ch++) {
+            if(temp.empty() || ch != temp.back()) {
+                temp.push_back(ch);
+                solve(idx+1 , n , k , res , temp);
+                temp.pop_back();
+            }
         }
     }
 
     string getHappyString(int n, int k) {
-        string curr = "";
+        vector<string> res;
 
-        string result = "";
-        
-        int count = 0;
-        
-        solve(n, curr, count, k, result);
+        string temp;
 
-        return result;
+        solve(0 , n , k , res , temp);
+
+        if(res.size() < k) return "";
+
+        return res[k-1];
     }
 };
