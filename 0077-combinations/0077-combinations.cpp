89abe5433361cc3f57973405950d6ca59b52ec50
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-20 13:52:08 +0530

    Time: 39 ms (58.75%), Space: 62.6 MB (65.77%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-
-    void recursion(vector<int> &res , int idx , vector<int> &temp , vector<vector<int>> &ans){
-        if(idx == res.size()){
-            ans.push_back(temp);
+    void solve(int idx , int n , int k , vector<vector<int>> &res , vector<int> &temp) {
+        if(k == 0) {
+            res.push_back(temp);
             return;
         }
 
-        //take
-        temp.push_back(res[idx]);
-        recursion(res , idx + 1 , temp , ans);
-        temp.pop_back();
-
-        //not-take
-        recursion(res , idx + 1 , temp , ans);
+        for(int i=idx; i<=n; i++) {
+            temp.push_back(i);
+            solve(i+1 , n , k-1 , res , temp);
+            temp.pop_back();
+        }
     }
 
     vector<vector<int>> combine(int n, int k) {
-        vector<int> res;
-        for(int i=1; i<=n; i++){
-            res.push_back(i);
-        }
+        vector<vector<int>> res;
 
         vector<int> temp;
-        vector<vector<int>> ans;
 
-        recursion(res , 0 , temp  , ans);
+        solve(1 , n , k , res , temp);
 
-        vector<vector<int>> final;
-
-        for(const auto &subset : ans){
-            if(subset.size() == k){
-                final.push_back(subset);
-            }
-        }
-        return final;
+        return res;
     }
 };
