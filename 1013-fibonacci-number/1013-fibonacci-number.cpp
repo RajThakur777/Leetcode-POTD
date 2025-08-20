7546f3c00662e6a9eaf00b8c8c02995353598f7e
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-20 19:43:50 +0530

    Time: 7 ms (52.38%), Space: 7.6 MB (87.31%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,14 +1,9 @@
+//Recursion:
 class Solution {
 public:
-    int fib(int n) {
-        if(n == 0 || n == 1){
-            return n;
-        }
+    int fib(int n) { 
+        if(n == 0 || n == 1) return n;
 
-        int fnm1 = fib(n-1);
-        int fnm2 = fib(n-2);
-        int fn = fnm1 + fnm2;
-
-        return fn;    
+        return fib(n-1) + fib(n-2);
     }
 };
