825a1181db64ddc766d5c19d5fa28ed2773a7894
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-22 21:53:27 +0530

    Time: 263 ms (41.89%), Space: 157.2 MB (39.31%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -25,27 +25,49 @@
 
 
 
-//Recursion + Memoization:
+// //Recursion + Memoization:
+// class Solution {
+// public:
+
+//     long long solve(int idx , vector<int> &nums , bool flag , vector<vector<long long>> &dp) {
+//         if(idx >= nums.size()) return 0;
+
+//         if(dp[idx][flag] != -1) return dp[idx][flag];
+
+//         long long pick = (flag ? nums[idx] + solve(idx+1 , nums , !flag , dp) : -nums[idx] + solve(idx+1 , nums , !flag , dp));
+
+//         long long not_pick = solve(idx+1 , nums , flag , dp);
+
+//         return dp[idx][flag] = max(pick , not_pick);
+//     }
+
+//     long long maxAlternatingSum(vector<int>& nums) {
+//         int n = nums.size();
+
+//         vector<vector<long long>> dp(n+1 , vector<long long>(2 , -1));
+
+//         return solve(0 , nums , true , dp);
+//     }
+// };
+
+
+
+
+
+
+
+//BottomUp -> Tabulation:
 class Solution {
-public:
-
-    long long solve(int idx , vector<int> &nums , bool flag , vector<vector<long long>> &dp) {
-        if(idx >= nums.size()) return 0;
-
-        if(dp[idx][flag] != -1) return dp[idx][flag];
-
-        long long pick = (flag ? nums[idx] + solve(idx+1 , nums , !flag , dp) : -nums[idx] + solve(idx+1 , nums , !flag , dp));
-
-        long long not_pick = solve(idx+1 , nums , flag , dp);
-
-        return dp[idx][flag] = max(pick , not_pick);
-    }
-
+public: 
     long long maxAlternatingSum(vector<int>& nums) {
         int n = nums.size();
 
-        vector<vector<long long>> dp(n+1 , vector<long long>(2 , -1));
+        vector<vector<long long>> dp(n+1 , vector<long long>(2));
 
-        return solve(0 , nums , true , dp);
+        for(int i=1; i<=n; i++) {
+            dp[i][0] = max(dp[i-1][1] - nums[i-1] , dp[i-1][0]);
+            dp[i][1] = max(dp[i-1][0] + nums[i-1] , dp[i-1][1]);
+        }
+        return max(dp[n][0] , dp[n][1]);
     }
 };
