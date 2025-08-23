94f8da95f7c47855c459aae4068f2347c9a39fae
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-23 10:18:22 +0530

    Time: 40 ms (53.48%), Space: 27.3 MB (55.21%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -27,35 +27,63 @@
 
 
 
-//Recursion + Memoization:
+// //Recursion + Memoization:
+// class Solution {
+// public:
+//     int solve(int idx , int prev , vector<vector<int>>& pairs , vector<vector<int>> &dp) {
+//         if(idx >= pairs.size()) return 0;
+
+//         if(prev != -1 && dp[idx][prev] != -1) return dp[idx][prev];
+
+//         int take = 0;
+//         if(prev == -1 || pairs[prev][1] < pairs[idx][0]) {
+//             take = 1 + solve(idx+1 , idx , pairs , dp);
+//         }
+
+//         int not_take = solve(idx+1 , prev , pairs , dp);
+
+//         if(prev != -1) {
+//             dp[idx][prev] = max(take , not_take);
+//         }
+
+//         return max(take , not_take);
+//     }
+
+//     int findLongestChain(vector<vector<int>>& pairs) {
+//         int n = pairs.size();
+
+//         sort(pairs.begin() , pairs.end());
+
+//         vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+
+//         return solve(0 , -1 , pairs , dp);
+//     }
+// };
+
+
+
+
+
+//Tabulation:
 class Solution {
 public:
-    int solve(int idx , int prev , vector<vector<int>>& pairs , vector<vector<int>> &dp) {
-        if(idx >= pairs.size()) return 0;
-
-        if(prev != -1 && dp[idx][prev] != -1) return dp[idx][prev];
-
-        int take = 0;
-        if(prev == -1 || pairs[prev][1] < pairs[idx][0]) {
-            take = 1 + solve(idx+1 , idx , pairs , dp);
-        }
-
-        int not_take = solve(idx+1 , prev , pairs , dp);
-
-        if(prev != -1) {
-            dp[idx][prev] = max(take , not_take);
-        }
-
-        return max(take , not_take);
-    }
-
     int findLongestChain(vector<vector<int>>& pairs) {
         int n = pairs.size();
 
         sort(pairs.begin() , pairs.end());
 
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+        vector<int> dp(n , 1);
 
-        return solve(0 , -1 , pairs , dp);
+        int ans = 1;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(pairs[j][1] < pairs[i][0]) {
+                    dp[i] = max(dp[i] , dp[j] + 1);
+                    ans = max(ans , dp[i]);
+                }
+            }
+        }
+        return ans;
     }
 };
