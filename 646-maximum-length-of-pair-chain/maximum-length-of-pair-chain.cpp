7fc9ed04d30fa0e77125a394e9aef6584f13355d
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-23 10:09:36 +0530

    Time: 283 ms (12.37%) | Memory: 163.6 MB (6.44%) - LeetSync

---
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -0,0 +1,61 @@
+// //Recursion:
+// class Solution {
+// public:
+//     int solve(int idx , int prev , vector<vector<int>>& pairs) {
+//         if(idx >= pairs.size()) return 0;
+
+//         int take = 0;
+//         if(prev == -1 || pairs[prev][1] < pairs[idx][0]) {
+//             take = 1 + solve(idx+1 , idx , pairs);
+//         }
+
+//         int not_take = solve(idx+1 , prev , pairs);
+
+//         return max(take , not_take);
+//     }
+
+//     int findLongestChain(vector<vector<int>>& pairs) {
+//         int n = pairs.size();
+
+//         sort(pairs.begin() , pairs.end());
+
+//         return solve(0 , -1 , pairs);
+//     }
+// };
+
+
+
+
+
+//Recursion + Memoization:
+class Solution {
+public:
+    int solve(int idx , int prev , vector<vector<int>>& pairs , vector<vector<int>> &dp) {
+        if(idx >= pairs.size()) return 0;
+
+        if(prev != -1 && dp[idx][prev] != -1) return dp[idx][prev];
+
+        int take = 0;
+        if(prev == -1 || pairs[prev][1] < pairs[idx][0]) {
+            take = 1 + solve(idx+1 , idx , pairs , dp);
+        }
+
+        int not_take = solve(idx+1 , prev , pairs , dp);
+
+        if(prev != -1) {
+            dp[idx][prev] = max(take , not_take);
+        }
+
+        return max(take , not_take);
+    }
+
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+
+        sort(pairs.begin() , pairs.end());
+
+        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+
+        return solve(0 , -1 , pairs , dp);
+    }
+};
