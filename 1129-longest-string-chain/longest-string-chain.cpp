485cf148653aea11c73c5098060ef68c3b4f4d70
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-23 10:59:05 +0530

    Time: 172 ms (31.59%) | Memory: 105.6 MB (33.78%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -0,0 +1,107 @@
+// //Recursion:
+// class Solution {
+// public:
+
+//     static bool cmp(string &s1 , string &s2) {
+//         return s1.size() < s2.size();
+//     }
+
+//     bool isValid(string &prev , string &curr) {
+//         int m = prev.size();
+//         int n = curr.size();
+
+//         if(m >= n || n-m != 1) return false;
+
+//         int i = 0;
+//         int j = 0;
+
+//         while(i < m && j < n) {
+//             if(prev[i] == curr[j]) {
+//                 i++;
+//             }
+//             j++;
+//         }
+//         return i == m;
+//     }
+
+//     int solve(int idx , int prev , vector<string>& words) {
+//         if(idx >= words.size()) return 0;
+
+//         int take = 0;
+//         if(prev == -1 || isValid(words[prev] , words[idx])) {
+//             take = 1 + solve(idx+1 , idx , words);
+//         }
+
+//         int not_take = solve(idx+1 , prev , words);
+
+//         return max(take , not_take);
+//     }
+
+//     int longestStrChain(vector<string>& words) {
+//         int n = words.size();
+
+//         sort(words.begin() , words.end() , cmp);
+
+//         return solve(0 , -1 , words);
+//     }
+// };
+
+
+
+
+//Recursion + Memoization:
+//Recursion:
+class Solution {
+public:
+
+    static bool cmp(string &s1 , string &s2) {
+        return s1.size() < s2.size();
+    }
+
+    bool isValid(string &prev , string &curr) {
+        int m = prev.size();
+        int n = curr.size();
+
+        if(m >= n || n-m != 1) return false;
+
+        int i = 0;
+        int j = 0;
+
+        while(i < m && j < n) {
+            if(prev[i] == curr[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i == m;
+    }
+
+    int solve(int idx , int prev , vector<string>& words , vector<vector<int>> &dp) {
+        if(idx >= words.size()) return 0;
+
+        if(prev != -1 && dp[idx][prev] != -1) return dp[idx][prev];
+
+        int take = 0;
+        if(prev == -1 || isValid(words[prev] , words[idx])) {
+            take = 1 + solve(idx+1 , idx , words , dp);
+        }
+
+        int not_take = solve(idx+1 , prev , words , dp);
+
+        if(prev != -1) {
+            dp[idx][prev] = max(take , not_take);
+        }
+
+        return max(take , not_take);
+    }
+
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+
+        sort(words.begin() , words.end() , cmp);
+
+        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+
+        return solve(0 , -1 , words , dp);
+    }
+};
