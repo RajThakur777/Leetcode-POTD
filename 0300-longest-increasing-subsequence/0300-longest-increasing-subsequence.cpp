a7eba075ffda228ba6d3ba35af671bb0c7913cab
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-23 09:43:23 +0530

    Time: 74 ms (55.83%), Space: 14.5 MB (52.03%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -25,33 +25,60 @@
 
 
 
-//Recursion + Memoization:
+// //Recursion + Memoization:
+// class Solution {
+// public:
+//     int solve(int idx , vector<int> &nums , int prev , vector<vector<int>> &dp) {
+//         if(idx >= nums.size()) return 0;
+
+//         if(prev != -1 && dp[idx][prev] != -1) return dp[idx][prev];
+
+//         int take = 0;
+//         if(prev == -1 || nums[prev] < nums[idx]) {
+//             take = 1 + solve(idx+1 , nums , idx , dp);
+//         }
+
+//         int not_take = solve(idx+1 , nums , prev , dp);
+
+//         if(prev != -1) {
+//             dp[idx][prev] = max(take , not_take);
+//         }
+
+//         return max(take , not_take);
+//     }
+
+//     int lengthOfLIS(vector<int>& nums) {  
+//         int n = nums.size();
+
+//         vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+
+//         return solve(0 , nums , -1 , dp);
+//     }
+// };
+
+
+
+
+
+
+//Tabulation:
 class Solution {
 public:
-    int solve(int idx , vector<int> &nums , int prev , vector<vector<int>> &dp) {
-        if(idx >= nums.size()) return 0;
-
-        if(prev != -1 && dp[idx][prev] != -1) return dp[idx][prev];
-
-        int take = 0;
-        if(prev == -1 || nums[prev] < nums[idx]) {
-            take = 1 + solve(idx+1 , nums , idx , dp);
-        }
-
-        int not_take = solve(idx+1 , nums , prev , dp);
-
-        if(prev != -1) {
-            dp[idx][prev] = max(take , not_take);
-        }
-
-        return max(take , not_take);
-    }
-
     int lengthOfLIS(vector<int>& nums) {  
         int n = nums.size();
 
-        vector<vector<int>> dp(n+1 , vector<int>(n+1 , -1));
+        vector<int> dp(n , 1);
 
-        return solve(0 , nums , -1 , dp);
+        int ans = 1;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<i; j++) {
+                if(nums[j] < nums[i]) {
+                    dp[i] = max(dp[i] , dp[j] + 1);
+                    ans = max(ans , dp[i]);
+                }
+            }
+        }
+        return ans;
     }
 };
