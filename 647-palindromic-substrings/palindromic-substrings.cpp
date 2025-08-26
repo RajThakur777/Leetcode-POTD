f20f4c9e7d12d765194835cbd79337340bdbd33b
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-26 15:16:54 +0530

    Time: 523 ms (16.03%) | Memory: 8.6 MB (81.38%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -0,0 +1,26 @@
+//Recursion-> brute force:
+class Solution {
+public:
+    bool check(string &s , int i , int j) {
+        if(i > j) return true;
+
+        if(s[i] == s[j]) {
+            return check(s , i+1 , j-1);
+        }
+        return false;
+    }
+
+    int countSubstrings(string s) {
+        int cnt = 0;
+        int n = s.size();
+
+        for(int i=0; i<n; i++) {
+            for(int j=i; j<n; j++) {
+                if(check(s , i , j)) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
