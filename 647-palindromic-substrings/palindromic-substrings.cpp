8c5fb1b37991211c9e00a5eb3ee22635f176ccc9
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-26 15:18:49 +0530

    Time: 984 ms (13.43%) | Memory: 12.9 MB (44.74%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,9 +1,44 @@
-//Recursion-> brute force:
+// //Recursion-> brute force:
+// class Solution {
+// public:
+//     bool check(string &s , int i , int j) {
+//         if(i > j) return true;
+
+//         if(s[i] == s[j]) {
+//             return check(s , i+1 , j-1);
+//         }
+//         return false;
+//     }
+
+//     int countSubstrings(string s) {
+//         int cnt = 0;
+//         int n = s.size();
+
+//         for(int i=0; i<n; i++) {
+//             for(int j=i; j<n; j++) {
+//                 if(check(s , i , j)) {
+//                     cnt++;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+
+
+//Recursion + Memoization:
 class Solution {
 public:
+    int dp[1001][1001];
+
     bool check(string &s , int i , int j) {
         if(i > j) return true;
 
+        if(dp[i][j] != -1) return dp[i][j];
+        
         if(s[i] == s[j]) {
             return check(s , i+1 , j-1);
         }
@@ -13,6 +48,8 @@ public:
     int countSubstrings(string s) {
         int cnt = 0;
         int n = s.size();
+
+        memset(dp , -1 , sizeof(dp));
 
         for(int i=0; i<n; i++) {
             for(int j=i; j<n; j++) {
