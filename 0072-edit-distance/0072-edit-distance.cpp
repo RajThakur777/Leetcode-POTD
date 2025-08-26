9c3ca7a656cebe598869e2d255097c2d05c18d60
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-26 11:20:00 +0530

    Time: 10 ms (34.73%), Space: 14.4 MB (17.68%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,72 @@
+// //Recursion:
+// class Solution {
+// public:
+//     int m;
+//     int n;
+
+//     int solve(string &word1 , string &word2 , int i , int j) {
+//         if(i == m) {
+//             return (n - j);
+//         }
+//         else if(j == n) {
+//             return (m - i);
+//         }
+
+//         if(word1[i] == word2[j]) {
+//             return solve(word1 , word2 , i+1 , j+1);
+//         }
+//         int op1 = 1 + solve(word1 , word2 , i+1 , j);
+//         int op2 = 1 + solve(word1 , word2 , i , j+1);
+//         int op3 = 1 + solve(word1 , word2 , i+1 , j+1);
+
+//         return min({op1 , op2 , op3});
+//     }
+
+//     int minDistance(string word1, string word2) { 
+//         m = word1.size();
+//         n = word2.size();
+
+//         return solve(word1 , word2 , 0 , 0);
+//     }
+// };
+
+
+
+
+
+
+//Recursion + Memoization:
+class Solution {
+public:
+    int m;
+    int n;
+
+    int solve(string &word1 , string &word2 , int i , int j , vector<vector<int>> &dp) {
+        if(i == m) {
+            return (n - j);
+        }
+        else if(j == n) {
+            return (m - i);
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(word1[i] == word2[j]) {
+            return dp[i][j] = solve(word1 , word2 , i+1 , j+1 , dp);
+        }
+        int op1 = 1 + solve(word1 , word2 , i+1 , j , dp);
+        int op2 = 1 + solve(word1 , word2 , i , j+1 , dp);
+        int op3 = 1 + solve(word1 , word2 , i+1 , j+1 , dp);
+
+        return dp[i][j] = min({op1 , op2 , op3});
+    }
+
+    int minDistance(string word1, string word2) { 
+        m = word1.size();
+        n = word2.size();
+
+        vector<vector<int>> dp(m+1 , vector<int>(n+1 , -1));
+
+        return solve(word1 , word2 , 0 , 0 , dp);
+    }
+};
