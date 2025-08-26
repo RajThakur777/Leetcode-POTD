0b31a666939d8defbe3e277ceab4d49091ee1fad
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-26 12:45:51 +0530

    Time: 7 ms (59.62%), Space: 13.1 MB (63.51%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -77,35 +77,68 @@
 
 
 
-//Recursion + Memoization:
+// //Recursion + Memoization:
+// class Solution {
+// public:
+//     int m;
+//     int n;
+
+//     int solve(string &word1 , string &word2 , int m , int n , vector<vector<int>> &dp) {
+//         if(m == 0 || n == 0) {
+//             return (m + n);
+//         }
+
+//         if(dp[m][n] != -1) return dp[m][n];
+
+//         if(word1[m-1] == word2[n-1]) {
+//             return dp[m][n] = solve(word1 , word2 , m-1 , n-1 , dp);
+//         }
+//         int op1 = 1 + solve(word1 , word2 , m , n-1 , dp);
+//         int op2 = 1 + solve(word1 , word2 , m-1 , n , dp);
+//         int op3 = 1 + solve(word1 , word2 , m-1 , n-1 , dp);
+
+//         return dp[m][n] = min({op1 , op2 , op3});
+//     }
+
+//     int minDistance(string word1, string word2) { 
+//         m = word1.size();
+//         n = word2.size();
+
+//         vector<vector<int>> dp(m+1 , vector<int>(n+1 , -1));
+
+//         return solve(word1 , word2 , m , n , dp);
+//     }
+// };
+
+
+
+
+
+
+
+
+//Tabulation:
 class Solution {
 public:
-    int m;
-    int n;
-
-    int solve(string &word1 , string &word2 , int m , int n , vector<vector<int>> &dp) {
-        if(m == 0 || n == 0) {
-            return (m + n);
-        }
-
-        if(dp[m][n] != -1) return dp[m][n];
-
-        if(word1[m-1] == word2[n-1]) {
-            return dp[m][n] = solve(word1 , word2 , m-1 , n-1 , dp);
-        }
-        int op1 = 1 + solve(word1 , word2 , m , n-1 , dp);
-        int op2 = 1 + solve(word1 , word2 , m-1 , n , dp);
-        int op3 = 1 + solve(word1 , word2 , m-1 , n-1 , dp);
-
-        return dp[m][n] = min({op1 , op2 , op3});
-    }
-
     int minDistance(string word1, string word2) { 
-        m = word1.size();
-        n = word2.size();
+        int m = word1.size();
+        int n = word2.size();
 
-        vector<vector<int>> dp(m+1 , vector<int>(n+1 , -1));
+        vector<vector<int>> dp(m+1 , vector<int>(n+1));
 
-        return solve(word1 , word2 , m , n , dp);
+        for(int i=0; i<m+1; i++) {
+            for(int j=0; j<n+1; j++) {
+                if((i == 0 || j == 0)) {
+                    dp[i][j] = (i+j);
+                }
+                else if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j] = 1 + min({dp[i][j-1] , dp[i-1][j] , dp[i-1][j-1]});
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
