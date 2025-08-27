3653f9cfd65ee3a293b96bf7c249c782da5deb7a
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-27 13:29:55 +0530

    Time: 24 ms (75.09%), Space: 31.3 MB (75.43%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -23,41 +23,57 @@
 
 
 
-//Recursion + Memoization:
+// //Recursion + Memoization:
+// class Solution {
+// public:
+//     int dp[501][501];
+    
+//     int solve(string &s , int i , int j) {
+//         if(i >= j) return 0;
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         if(s[i] == s[j]) {
+//             return dp[i][j] = solve(s , i+1 , j-1);
+//         }
+//         else {
+//             return dp[i][j] = 1 + min(solve(s , i+1 , j) , solve(s , i , j-1));
+//         }
+//     }
+
+//     int minInsertions(string s) {
+//         int n = s.size();
+
+//         memset(dp , -1 , sizeof(dp));
+
+//         return solve(s , 0 , n-1);
+//     }
+// };
+
+
+
+
+
+
+//Tabulation:
 class Solution {
 public:
-    int dp[501][501];
-    
-    int solve(string &s , int i , int j) {
-        if(i >= j) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i] == s[j]) {
-            return dp[i][j] = solve(s , i+1 , j-1);
-        }
-        else {
-            return dp[i][j] = 1 + min(solve(s , i+1 , j) , solve(s , i , j-1));
-        }
-    }
-
     int minInsertions(string s) {
         int n = s.size();
 
-        memset(dp , -1 , sizeof(dp));
+        vector<vector<int>> dp(n , vector<int>(n , 0));
 
-        return solve(s , 0 , n-1);
+        for(int L=2; L<=n; L++) {
+            for(int i=0; i+L-1<n; i++) {
+                int j = i+L-1;
+                if(s[i] == s[j]) {
+                    dp[i][j] = dp[i+1][j-1];
+                }
+                else {
+                    dp[i][j] = 1 + min(dp[i][j-1] , dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 };
-
-
-
-
-
-
-// //Tabulation:
-// class Solution {
-// public:
-//     int minInsertions(string s) {
-//     }
-// };
