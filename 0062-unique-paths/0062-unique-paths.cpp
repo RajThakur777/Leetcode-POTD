42e710dcac8caee676dd288a654a182244d34c4b
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-28 16:07:36 +0530

    Time: 0 ms (100%), Space: 8 MB (87.35%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,49 +1,48 @@
 // //Recursion:
 // class Solution {
 // public:
-
-//     int helper(int i , int j , int m , int n){
-
+   
+//     int solve(int i , int j , int m , int n) {
 //         if(i == m-1 && j == n-1) return 1;
 
-//         if(i >= m || j >= n) return 0;
+//         if(i < 0 || i >= m || j < 0 || j >= n) return 0;
 
-//         return helper(i+1 , j , m , n) + helper(i , j+1 , m , n);
+//         int right = solve(i , j+1 , m , n);
+//         int down = solve(i+1 , j , m , n);
+
+//         return right + down;
 //     }
 
 //     int uniquePaths(int m, int n) {
-//         int ans = helper(0 , 0 , m , n);
-
-//         return ans;
+//         return solve(0 , 0 , m , n);
 //     }
 // };
 
 
 
 
-//Memoization:
-//Recursion:
+
+//Recursion + Memoization:
 class Solution {
 public:
-    int helper(int i , int j , int m , int n , vector<vector<int>> &dp){
-        if(i >= m || j >= n) return 0;
+    int dp[101][101];
 
+    int solve(int i , int j , int m , int n) {
         if(i == m-1 && j == n-1) return 1;
+
+        if(i < 0 || i >= m || j < 0 || j >= n) return 0;
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        return dp[i][j] = helper(i+1 , j , m , n , dp) + helper(i , j+1 , m , n , dp);
+        int right = solve(i , j+1 , m , n);
+        int down = solve(i+1 , j , m , n);
+
+        return dp[i][j] = right + down;
     }
 
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m , vector<int> (n));
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                dp[i][j] = -1;
-            }
-        }
-        int ans = helper(0 , 0 , m , n , dp);
+        memset(dp , -1 , sizeof(dp));
 
-        return ans;
+        return solve(0 , 0 , m , n);
     }
 };
