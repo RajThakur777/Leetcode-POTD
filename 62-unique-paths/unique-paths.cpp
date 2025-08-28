9a07c4c3686bc247e89187d7d63b1454179d717e
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-28 16:21:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (70.17%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -22,27 +22,57 @@
 
 
 
-//Recursion + Memoization:
+// //Recursion + Memoization:
+// class Solution {
+// public:
+//     int dp[101][101];
+
+//     int solve(int i , int j , int m , int n) {
+//         if(i == m-1 && j == n-1) return 1;
+
+//         if(i < 0 || i >= m || j < 0 || j >= n) return 0;
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int right = solve(i , j+1 , m , n);
+//         int down = solve(i+1 , j , m , n);
+
+//         return dp[i][j] = right + down;
+//     }
+
+//     int uniquePaths(int m, int n) {
+//         memset(dp , -1 , sizeof(dp));
+
+//         return solve(0 , 0 , m , n);
+//     }
+// };
+
+
+
+
+
+
+
+//Tabulation
 class Solution {
 public:
-    int dp[101][101];
-
-    int solve(int i , int j , int m , int n) {
-        if(i == m-1 && j == n-1) return 1;
-
-        if(i < 0 || i >= m || j < 0 || j >= n) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int right = solve(i , j+1 , m , n);
-        int down = solve(i+1 , j , m , n);
-
-        return dp[i][j] = right + down;
-    }
-
     int uniquePaths(int m, int n) {
-        memset(dp , -1 , sizeof(dp));
+        vector<vector<int>> dp(m , vector<int>(n));
 
-        return solve(0 , 0 , m , n);
+        dp[0][0] = 1;
+        for(int col=1; col<n; col++) {
+            dp[0][col] = 1;
+        }
+
+        for(int row=1; row<m; row++) {
+            dp[row][0] = 1;
+        }
+
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
