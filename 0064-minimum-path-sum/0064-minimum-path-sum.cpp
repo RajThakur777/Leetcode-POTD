f2351edb13bec9a3e14475580d7c11206ff2dc9d
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-31 15:24:20 +0530

    Time: 0 ms (100%), Space: 16.3 MB (62.41%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -26,31 +26,64 @@
 
 
 
-//Recursion + Memoization:
+// //Recursion + Memoization:
+// class Solution {
+// public:
+//     int m, n;
+//     int dp[201][201];
+
+//     int solve(int i, int j, vector<vector<int>>& grid) {
+//         if (i >= m || j >= n) return INT_MAX;
+
+//         if (i == m-1 && j == n-1) return grid[i][j];
+
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int right = solve(i, j+1, grid);
+//         int down  = solve(i+1, j, grid);
+
+//         return dp[i][j] = grid[i][j] + min(right, down);
+//     }
+
+//     int minPathSum(vector<vector<int>>& grid) {
+//         m = grid.size();
+//         n = grid[0].size();
+
+//         memset(dp , -1 , sizeof(dp));
+
+//         return solve(0, 0, grid);
+//     }
+// };
+
+
+
+
+
+
+//Tabulation:
 class Solution {
 public:
-    int m, n;
-    int dp[201][201];
-
-    int solve(int i, int j, vector<vector<int>>& grid) {
-        if (i >= m || j >= n) return INT_MAX;
-
-        if (i == m-1 && j == n-1) return grid[i][j];
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int right = solve(i, j+1, grid);
-        int down  = solve(i+1, j, grid);
-
-        return dp[i][j] = grid[i][j] + min(right, down);
-    }
-
     int minPathSum(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        memset(dp , -1 , sizeof(dp));
+        vector<vector<int>> dp(m+1 , vector<int>(n+1));
 
-        return solve(0, 0, grid);
+        dp[0][0] = grid[0][0];
+
+        for(int i=1; i<m; i++) {
+            dp[i][0] = grid[i][0] + dp[i-1][0];
+        }
+
+        for(int j=1; j<n; j++) {
+            dp[0][j] = grid[0][j] + dp[0][j-1];
+        }
+
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = grid[i][j] + min(dp[i-1][j] , dp[i][j-1]);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
