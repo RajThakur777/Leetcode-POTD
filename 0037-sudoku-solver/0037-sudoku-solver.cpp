daee73cb036a12f59e6e7b08500686abfe2e9b0f
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-08-31 20:40:25 +0530

    Time: 245 ms (71.68%), Space: 8.8 MB (62.78%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
-
-    bool check(vector<vector<char>>& board , int r , int c , char ch){
-        //checking for present row:
-        for(int i=0; i<9; i++){
-            if(board[r][i] == ch) return false;
+    bool isRow(vector<vector<char>>& board , int r , char ch) {
+        for(int col=0; col<9; col++) {
+            if(board[r][col] == ch) return false;
         }
+        return true;
+    }
 
-        //checking for present column:
-        for(int i=0; i<9; i++){
-            if(board[i][c] == ch) return false;
+    bool isCol(vector<vector<char>>& board , int c , char ch) {
+        for(int row=0; row<9; row++) {
+            if(board[row][c] == ch) return false;
         }
+        return true;
+    }
 
-        //checking for 3*3 grid:
-        int r1 = r - r % 3;
-        int c1 = c - c % 3;
-
-        for(int i=r1; i<r1+3; i++){
-            for(int j=c1; j<c1+3; j++){
+    bool isThree(vector<vector<char>>& board , int r , int c , char ch) {
+        int sr = r - (r % 3);
+        int sc = c - (c % 3);
+        for(int i=sr; i<sr+3; i++) {
+            for(int j=sc; j<sc+3; j++) {
                 if(board[i][j] == ch) return false;
             }
         }
         return true;
     }
 
-    bool helper(vector<vector<char>>& board , int r , int c){
-        if(r == 9) return true;
+    bool isValid(vector<vector<char>>& board , int r , int c , char ch) {
+        return (isRow(board , r , ch) && isCol(board , c , ch) && isThree(board , r , c , ch));
+    }
 
-        if(c == 9) return helper(board , r+1 , 0);
-
-        //not a empty cell:
-        if(board[r][c] != '.'){
-            return helper(board , r , c+1);
-        }
-
-        //empty cell:
-        for(char ch='1'; ch<='9'; ch++){
-            if(check(board , r , c , ch) == false) continue;
-            board[r][c] = ch;
-            bool ans = helper(board , r , c+1);
-            if(ans == true){
-                return true;
+    bool solve(vector<vector<char>>& board) {
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                if(board[i][j] == '.') {
+                    for(char d='1'; d<='9'; d++) {
+                        if(isValid(board , i , j , d)) {
+                            board[i][j] = d;
+                            if(solve(board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
             }
-            board[r][c] = '.';
         }
-        return false;
+        return true;
     }
 
     void solveSudoku(vector<vector<char>>& board) {
-        bool ans = helper(board , 0 , 0);
+        solve(board);
     }
 };
