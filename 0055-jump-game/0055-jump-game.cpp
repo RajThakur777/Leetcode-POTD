a75a17ecbe7e3fb81055df4b5877841c247be052
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-05 13:16:13 +0530

    Time: 48 ms (19.24%), Space: 53.2 MB (15.37%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -26,31 +26,58 @@
 
 
 
-//Recursion+Memoization:
+// //Recursion+Memoization:
+// class Solution {
+// public:
+//     int dp[10001];
+
+//     bool solve(int idx , vector<int> &nums) {
+//         if(idx >= nums.size()) return false;
+
+//         if(idx == nums.size() - 1) return true;
+
+//         if(nums[idx] == 0) return false;
+
+//         if(dp[idx] != -1) return dp[idx];
+
+//         for(int i=1; i<=nums[idx]; i++) {
+//             if(solve(idx+i , nums) == true) {
+//                 return dp[idx] = true;
+//             }
+//         }
+//         return dp[idx] = false;
+//     }
+
+//     bool canJump(vector<int>& nums) {
+//         memset(dp , -1 , sizeof(dp));
+
+//         return solve(0 , nums);
+//     }
+// };
+
+
+
+
+
+
+
+//Tabulation:
 class Solution {
 public:
-    int dp[10001];
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
 
-    bool solve(int idx , vector<int> &nums) {
-        if(idx >= nums.size()) return false;
+        vector<bool> dp(n , false);
+        dp[0] = true;
 
-        if(idx == nums.size() - 1) return true;
-
-        if(nums[idx] == 0) return false;
-
-        if(dp[idx] != -1) return dp[idx];
-
-        for(int i=1; i<=nums[idx]; i++) {
-            if(solve(idx+i , nums) == true) {
-                return dp[idx] = true;
+        for(int i=1; i<n; i++) {
+            for(int j=i-1; j>=0; j--) {
+                if(dp[j] == true && (j + nums[j] >= i)) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        return dp[idx] = false;
-    }
-
-    bool canJump(vector<int>& nums) {
-        memset(dp , -1 , sizeof(dp));
-
-        return solve(0 , nums);
+        return dp[n-1];
     }
 };
