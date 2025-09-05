89f29a5ee3b4161e5a98fb393a64022ee3a99c79
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-05 12:54:48 +0530

    Time: 692 ms (5.95%) | Memory: 53.4 MB (13.39%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,56 @@
+// //Recursion:
+// class Solution {
+// public:
+
+//     bool solve(int idx , vector<int> &nums) {
+//         if(idx >= nums.size()) return false;
+
+//         if(idx == nums.size() - 1) return true;
+
+//         if(nums[idx] == 0) return false;
+
+//         for(int i=1; i<=nums[idx]; i++) {
+//             if(solve(idx+i , nums) == true) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     bool canJump(vector<int>& nums) {
+//         return solve(0 , nums);
+//     }
+// };
+
+
+
+
+
+//Recursion+Memoization:
+class Solution {
+public:
+    int dp[10001];
+
+    bool solve(int idx , vector<int> &nums) {
+        if(idx >= nums.size()) return false;
+
+        if(idx == nums.size() - 1) return true;
+
+        if(nums[idx] == 0) return false;
+
+        if(dp[idx] != -1) return dp[idx];
+
+        for(int i=1; i<=nums[idx]; i++) {
+            if(solve(idx+i , nums) == true) {
+                return dp[idx] = true;
+            }
+        }
+        return dp[idx] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        memset(dp , -1 , sizeof(dp));
+
+        return solve(0 , nums);
+    }
+};
