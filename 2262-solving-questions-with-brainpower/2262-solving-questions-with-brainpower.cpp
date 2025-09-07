08b75b3882a097619bae893c9501eab0f9720221
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-07 12:30:33 +0530

    Time: 24 ms (7.05%), Space: 211.7 MB (5.01%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -23,28 +23,47 @@
 
 
 
-//Recursion+Memoization:
+// //Recursion+Memoization:
+// class Solution {
+// public:
+//     long long n;
+
+//     long long solve(int idx , vector<vector<int>>& questions , vector<long long> &dp) {
+//         if(idx >= n) return 0;
+
+//         if(dp[idx] != -1) return dp[idx];
+
+//         long long pick = questions[idx][0] + solve(idx+questions[idx][1]+1 , questions , dp);
+
+//         long long not_pick = solve(idx+1 , questions , dp);
+
+//         return dp[idx] = max(pick , not_pick);
+//     }
+
+//     long long mostPoints(vector<vector<int>>& questions) {
+//         n = questions.size();
+
+//         vector<long long> dp(n , -1);
+
+//         return solve(0 , questions , dp);
+//     }
+// };
+
+
+
+
+
+//Tabulation:
 class Solution {
 public:
-    long long n;
-
-    long long solve(int idx , vector<vector<int>>& questions , vector<long long> &dp) {
-        if(idx >= n) return 0;
-
-        if(dp[idx] != -1) return dp[idx];
-
-        long long pick = questions[idx][0] + solve(idx+questions[idx][1]+1 , questions , dp);
-
-        long long not_pick = solve(idx+1 , questions , dp);
-
-        return dp[idx] = max(pick , not_pick);
-    }
-
     long long mostPoints(vector<vector<int>>& questions) {
-        n = questions.size();
+        long long n = questions.size();
 
-        vector<long long> dp(n , -1);
+        vector<long long> dp(200001 , 0);
 
-        return solve(0 , questions , dp);
+        for(int i=n-1; i>=0; i--) {
+            dp[i] = max(questions[i][0] + dp[i + questions[i][1] + 1] , dp[i+1]);
+        }
+        return dp[0];
     }
 };
