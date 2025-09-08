edaf052aafb3ab976056ebe40f1cd0fd88954c21
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-08 09:56:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (69.33%) - LeetSync

---
+++ 343-integer-break/integer-break.cpp
@@ -0,0 +1,51 @@
+// //Recursion:
+// class Solution {
+// public:
+
+//     int solve(int n) {
+//         if(n == 1) return 1;
+
+//         int ans = INT_MIN;
+
+//         for(int i=1; i<=n-1; i++) {
+//             int prod = i * max(n-i , solve(n-i));
+
+//             ans = max(ans , prod);
+//         }
+//         return ans;
+//     }
+
+//     int integerBreak(int n) {
+//         return solve(n);
+//     }
+// };
+
+
+
+
+//Recursion+Memoization:
+class Solution {
+public:
+    int dp[59];
+
+    int solve(int n) {
+        if(n == 1) return 1;
+
+        if(dp[n] != -1) return dp[n];
+
+        int ans = INT_MIN;
+        
+        for(int i=1; i<=n-1; i++) {
+            int prod = i * max(n-i , solve(n-i));
+
+            ans = max(ans , prod);
+        }
+        return dp[n] = ans;
+    }
+
+    int integerBreak(int n) {
+        memset(dp , -1 , sizeof(dp));
+
+        return solve(n);
+    }
+};
