5bb3b580eee909f52b8bedf94ea057ffbd3a7afd
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-10 11:43:29 +0530

    Time: 968 ms (5.06%), Space: 11.7 MB (75.14%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,20 @@
+//Recursion:
 class Solution {
 public:
 
-    int helper(int idx , vector<int> &nums , int target , int sum){
-        if(idx == nums.size()){
-            return sum == target;
+    int solve(int idx , vector<int> &nums , int target , int sum) {
+        if(idx >= nums.size()) {
+            return (sum == target);
         }
-        //plus
-        int plus = helper(idx + 1 , nums , target , sum + nums[idx]);
 
-        //minus
-        int minus = helper(idx + 1 , nums , target , sum - nums[idx]);
+        int plus = solve(idx+1 , nums , target , sum + nums[idx]);
 
-        return plus + minus;
+        int minus = solve(idx+1 , nums , target , sum - nums[idx]);
+
+        return (plus + minus);
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        return helper(0 , nums , target , 0);
+        return solve(0 , nums , target , 0);
     }
 };
