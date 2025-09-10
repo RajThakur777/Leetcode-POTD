98cc1749cd252d3a2c23d42870c2efe245bf6bb2
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-10 12:31:35 +0530

    Time: 15 ms (52.9%), Space: 26.3 MB (22.57%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,20 +1,55 @@
-//Recursion:
+// //Recursion:
+// class Solution {
+// public:
+
+//     int solve(int idx , vector<int> &nums , int target , int sum) {
+//         if(idx >= nums.size()) {
+//             return (sum == target);
+//         }
+
+//         int plus = solve(idx+1 , nums , target , sum + nums[idx]);
+
+//         int minus = solve(idx+1 , nums , target , sum - nums[idx]);
+
+//         return (plus + minus);
+//     }
+
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         return solve(0 , nums , target , 0);
+//     }
+// };
+
+
+
+
+//Recursion+Memoization:
 class Solution {
 public:
-
-    int solve(int idx , vector<int> &nums , int target , int sum) {
+    int S;
+    int solve(int idx , vector<int> &nums , int target , int sum , vector<vector<int>> &dp) {
         if(idx >= nums.size()) {
             return (sum == target);
         }
 
-        int plus = solve(idx+1 , nums , target , sum + nums[idx]);
+        if(dp[idx][sum+S] != -1) return dp[idx][sum+S];
 
-        int minus = solve(idx+1 , nums , target , sum - nums[idx]);
+        int plus = solve(idx+1 , nums , target , sum + nums[idx] , dp);
 
-        return (plus + minus);
+        int minus = solve(idx+1 , nums , target , sum - nums[idx] , dp);
+
+        return dp[idx][sum+S] = (plus + minus);
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(0 , nums , target , 0);
+        int n = nums.size();
+
+        S = 0;
+        for(int i=0; i<n; i++) {
+            S += nums[i];
+        }
+
+        vector<vector<int>> dp(n+1 , vector<int>(2*S+1 , -1));
+
+        return solve(0 , nums , target , 0 , dp);
     }
 };
