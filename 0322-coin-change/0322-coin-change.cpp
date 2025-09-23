fc7bcbdbb06d1c9ede8360e58e6ec08689882b99
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-23 10:29:12 +0530

    Time: 22 ms (73.17%), Space: 13.5 MB (97.24%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,20 +1,38 @@
+// //Recursion:
 // class Solution {
 // public:
+//     int n;
 
-//     int helper(int idx , vector<int> &coins , int amount){
-//         if(amount == 0) return 0;
+//     int solve(int idx , vector<int> &coins , int amount) {
+//         if(idx < 0 || amount < 0) return 1e9;
 
-//         if(idx < 0 || amount < 0) return INT_MAX / 2;
+//         if(idx == 0) {
+//             if(amount % coins[idx] == 0) {
+//                 return (amount / coins[idx]);
+//             }
+//             else {
+//                 return 1e9;
+//             }
+//         }
 
-//         return min(helper(idx , coins , amount-coins[idx]) + 1 , helper(idx-1 , coins , amount));
+//         int not_pick = solve(idx-1 , coins , amount);
+
+//         int pick = 1e9;
+//         if(coins[idx] <= amount) {
+//             pick = 1 + solve(idx , coins , amount-coins[idx]);
+//         }
+
+//         return min(pick , not_pick);
 //     }
 
-//     int coinChange(vector<int>& coins, int amount) {
-//         int n = coins.size();
+//     int coinChange(vector<int>& coins, int amount) { 
+//         n = coins.size();
 
-//         int ans = helper(n-1 , coins , amount);
+//         int ans = solve(n-1 , coins , amount);
 
-//         return (ans >= INT_MAX / 2) ? -1 : ans;
+//         if(ans == 1e9) return -1;
+
+//         return ans;
 //     }
 // };
 
@@ -22,27 +40,45 @@
 
 
 
-//Memoization:
+//Recursion+Memoization:
 class Solution {
 public:
+    int n;
+    int dp[13][10001];
 
-    int helper(int idx , vector<int> &coins , int amount , vector<vector<int>> &dp){
-        if(amount == 0) return 0;
+    int solve(int idx , vector<int> &coins , int amount) {
+        if(idx < 0 || amount < 0) return 1e9;
 
-        if(idx < 0 || amount < 0) return INT_MAX / 2;
+        if(idx == 0) {
+            if(amount % coins[idx] == 0) {
+                return (amount / coins[idx]);
+            }
+            else {
+                return 1e9;
+            }
+        }
 
         if(dp[idx][amount] != -1) return dp[idx][amount];
 
-        return dp[idx][amount] = min(helper(idx , coins , amount-coins[idx] , dp) + 1 , helper(idx-1 , coins , amount , dp));
+        int not_pick = solve(idx-1 , coins , amount);
+
+        int pick = 1e9;
+        if(coins[idx] <= amount) {
+            pick = 1 + solve(idx , coins , amount-coins[idx]);
+        }
+
+        return dp[idx][amount] = min(pick , not_pick);
     }
 
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
+    int coinChange(vector<int>& coins, int amount) { 
+        n = coins.size();
 
-        vector<vector<int>> dp(n , vector<int> (amount+1 , -1));
+        memset(dp , -1 , sizeof(dp));
 
-        int ans = helper(n-1 , coins , amount , dp);
+        int ans = solve(n-1 , coins , amount);
 
-        return (ans >= INT_MAX / 2) ? -1 : ans;
+        if(ans == 1e9) return -1;
+
+        return ans;
     }
 };
