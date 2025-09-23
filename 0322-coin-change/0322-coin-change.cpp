b0e7eafcbe09e014858bc35d48ba1515a7392dd2
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-23 10:45:33 +0530

    Time: 58 ms (16.03%), Space: 52.9 MB (14.98%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -40,45 +40,82 @@
 
 
 
-//Recursion+Memoization:
+// //Recursion+Memoization:
+// class Solution {
+// public:
+//     int n;
+//     int dp[13][10001];
+
+//     int solve(int idx , vector<int> &coins , int amount) {
+//         if(idx < 0 || amount < 0) return 1e9;
+
+//         if(idx == 0) {
+//             if(amount % coins[idx] == 0) {
+//                 return (amount / coins[idx]);
+//             }
+//             else {
+//                 return 1e9;
+//             }
+//         }
+
+//         if(dp[idx][amount] != -1) return dp[idx][amount];
+
+//         int not_pick = solve(idx-1 , coins , amount);
+
+//         int pick = 1e9;
+//         if(coins[idx] <= amount) {
+//             pick = 1 + solve(idx , coins , amount-coins[idx]);
+//         }
+
+//         return dp[idx][amount] = min(pick , not_pick);
+//     }
+
+//     int coinChange(vector<int>& coins, int amount) { 
+//         n = coins.size();
+
+//         memset(dp , -1 , sizeof(dp));
+
+//         int ans = solve(n-1 , coins , amount);
+
+//         if(ans == 1e9) return -1;
+
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+//Tabulation:
 class Solution {
 public:
-    int n;
-    int dp[13][10001];
-
-    int solve(int idx , vector<int> &coins , int amount) {
-        if(idx < 0 || amount < 0) return 1e9;
-
-        if(idx == 0) {
-            if(amount % coins[idx] == 0) {
-                return (amount / coins[idx]);
-            }
-            else {
-                return 1e9;
-            }
-        }
-
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-
-        int not_pick = solve(idx-1 , coins , amount);
-
-        int pick = 1e9;
-        if(coins[idx] <= amount) {
-            pick = 1 + solve(idx , coins , amount-coins[idx]);
-        }
-
-        return dp[idx][amount] = min(pick , not_pick);
-    }
-
     int coinChange(vector<int>& coins, int amount) { 
-        n = coins.size();
+        int n = coins.size();
 
-        memset(dp , -1 , sizeof(dp));
+        vector<vector<int>> dp(n+1 , vector<int>(amount+1));
 
-        int ans = solve(n-1 , coins , amount);
+        for(int i=0; i<=n; i++) {
+            dp[i][0] = 0;
+        }
 
-        if(ans == 1e9) return -1;
+        for(int t=0; t<=amount; t++) {
+            dp[0][t] = ((amount % coins[0]) == 0 ? (amount / coins[0]) : 1e9);
+        }
 
-        return ans;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=amount; j++) {
+                int not_pick = dp[i-1][j];
+
+                int pick = 1e9;
+
+                if(j >= coins[i-1]) {
+                    pick = 1 + dp[i][j-coins[i-1]];
+                }
+                dp[i][j] = min(pick , not_pick);
+            }
+        }
+        return dp[n][amount] == 1e9 ? -1 : dp[n][amount];
     }
 };
