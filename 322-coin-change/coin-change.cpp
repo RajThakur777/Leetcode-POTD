af7bbb1c7ddbdf24da61928943780eb0c2d7be0f
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-23 10:29:17 +0530

    Time: 22 ms (73.17%) | Memory: 13.5 MB (97.24%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,84 @@
+// //Recursion:
+// class Solution {
+// public:
+//     int n;
+
+//     int solve(int idx , vector<int> &coins , int amount) {
+//         if(idx < 0 || amount < 0) return 1e9;
+
+//         if(idx == 0) {
+//             if(amount % coins[idx] == 0) {
+//                 return (amount / coins[idx]);
+//             }
+//             else {
+//                 return 1e9;
+//             }
+//         }
+
+//         int not_pick = solve(idx-1 , coins , amount);
+
+//         int pick = 1e9;
+//         if(coins[idx] <= amount) {
+//             pick = 1 + solve(idx , coins , amount-coins[idx]);
+//         }
+
+//         return min(pick , not_pick);
+//     }
+
+//     int coinChange(vector<int>& coins, int amount) { 
+//         n = coins.size();
+
+//         int ans = solve(n-1 , coins , amount);
+
+//         if(ans == 1e9) return -1;
+
+//         return ans;
+//     }
+// };
+
+
+
+
+
+//Recursion+Memoization:
+class Solution {
+public:
+    int n;
+    int dp[13][10001];
+
+    int solve(int idx , vector<int> &coins , int amount) {
+        if(idx < 0 || amount < 0) return 1e9;
+
+        if(idx == 0) {
+            if(amount % coins[idx] == 0) {
+                return (amount / coins[idx]);
+            }
+            else {
+                return 1e9;
+            }
+        }
+
+        if(dp[idx][amount] != -1) return dp[idx][amount];
+
+        int not_pick = solve(idx-1 , coins , amount);
+
+        int pick = 1e9;
+        if(coins[idx] <= amount) {
+            pick = 1 + solve(idx , coins , amount-coins[idx]);
+        }
+
+        return dp[idx][amount] = min(pick , not_pick);
+    }
+
+    int coinChange(vector<int>& coins, int amount) { 
+        n = coins.size();
+
+        memset(dp , -1 , sizeof(dp));
+
+        int ans = solve(n-1 , coins , amount);
+
+        if(ans == 1e9) return -1;
+
+        return ans;
+    }
+};
