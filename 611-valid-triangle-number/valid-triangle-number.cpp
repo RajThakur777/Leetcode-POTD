3bd6e19ec5decfd161aea04a74421b1519785ddc
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-09-26 16:17:16 +0530

    Time: 119 ms (26.59%) | Memory: 16.6 MB (42.88%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -1,5 +1,51 @@
+// //Brute force:
+// class Solution {
+// public:
+//     int triangleNumber(vector<int>& nums) {
+//         int n = nums.size();
+
+//         sort(nums.begin() , nums.end());
+
+//         int cnt = 0;
+
+//         for(int i=0; i<n-2; i++) {
+//             for(int j=i+1; j<n-1; j++) {
+//                 for(int k=j+1; k<n; k++) {
+//                     if(((nums[i] + nums[j]) > nums[k]) && ((nums[j] + nums[k]) > nums[i]) && ((nums[i] + nums[k]) > nums[j])) {
+//                         cnt++;
+//                     }
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+
+
+
+
+//Using Binary Search:
 class Solution {
 public:
+
+    int solve(int l , int r , int k , vector<int> &nums) {
+        int ans = 0;
+
+        while(l <= r) {
+            int mid = (l + r) / 2;
+
+            if(nums[mid] < k) {
+                ans = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
     int triangleNumber(vector<int>& nums) {
         int n = nums.size();
 
@@ -7,12 +53,12 @@ public:
 
         int cnt = 0;
 
-        for(int i=0; i<n-2; i++) {
-            for(int j=i+1; j<n-1; j++) {
-                for(int k=j+1; k<n; k++) {
-                    if(((nums[i] + nums[j]) > nums[k]) && ((nums[j] + nums[k]) > nums[i]) && ((nums[i] + nums[k]) > nums[j])) {
-                        cnt++;
-                    }
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                int res = solve(j+1 , n-1 , nums[i]+nums[j] , nums);
+
+                if(res != 0) {
+                    cnt += (res - j);
                 }
             }
         }
