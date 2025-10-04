e1ee5457e7fc700497c6411864adef99fb5f27f0
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-10-04 14:11:30 +0530

    Time: 3 ms (65.88%), Space: 10 MB (91.79%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,46 +1,53 @@
 class Solution {
 public:
-    bool check(int r , int c , vector<string> &v , int n){
-        //check vertical direction:
-        for(int i=r; i>=0; i--){
-            if(v[i][c] == 'Q') return false;
+    int N;
+    bool is_Safe(int i , int j , vector<string> &temp) {
+        int row = i;
+        int col = j;
+
+        for(int l=row-1; l>=0; l--) {
+            if(temp[l][col] == 'Q') {
+                return false;
+            }
         }
 
-        //check left diagonal:
-        for(int i=r,j=c; i>=0 && j>=0; i--,j--){
-            if(v[i][j] == 'Q') return false;
+        for(int l=row-1,r=col-1; l>=0 && r>=0; l--,r--) {
+            if(temp[l][r] == 'Q') {
+                return false;
+            }
         }
 
-        //check right diagonal:
-        for(int i=r,j=c; i>=0 && j<n; i--,j++){
-            if(v[i][j] == 'Q') return false;
+        for(int l=row-1,r=col+1; l>=0 && r<temp.size(); l--,r++) {
+            if(temp[l][r] == 'Q') {
+                return false;
+            }
         }
         return true;
     }
 
-    void helper(int r , vector<vector<string>> &ans , vector<string> &v , int n){
-        if(r == n){
-            ans.push_back(v);
+    void solve(int row , vector<vector<string>> &ans , vector<string> &temp) {
+        if(row == N) {
+            ans.push_back(temp);
             return;
         }
 
-        for(int i=0; i<n; i++){
-            if(check(r , i , v , n) == false) continue;
-
-            v[r][i] = 'Q';
-            helper(r+1 , ans , v , n);
-            v[r][i] = '.';
+        for(int col=0; col<temp.size(); col++) {
+            if(is_Safe(row , col , temp)) {
+                temp[row][col] = 'Q';
+                solve(row+1 , ans , temp);
+                temp[row][col] = '.';
+            }
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
 
-        string temp(n , '.');
+        N = n;
 
-        vector<string> v(n , temp);
-
-        helper(0 , ans , v , n);
+        vector<string> temp(n, string(n, '.'));
+        
+        solve(0 , ans , temp);
 
         return ans;
     }
