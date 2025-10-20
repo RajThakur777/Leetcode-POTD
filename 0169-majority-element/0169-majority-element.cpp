400601ae6863fb8b1e73f6b186d4b7cb38239f7d
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-10-20 09:46:31 +0530

    Time: 0 ms (100%), Space: 28.4 MB (7.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,13 +4,14 @@ public:
         int n = nums.size();
 
         map<int , int> mpp;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++) {
             mpp[nums[i]]++;
-        }  
+        }
 
         int ans = 0;
-        for(auto it : mpp){
-            if(it.second > n / 2){
+        
+        for(auto it : mpp) {
+            if(it.second > (n / 2)) {
                 ans = it.first;
             }
         }
