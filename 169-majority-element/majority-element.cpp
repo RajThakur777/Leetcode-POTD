d42d02a50cc87ba21de53666876971ee710ee059
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-10-20 09:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (7.79%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+
+        map<int , int> mpp;
+        for(int i=0; i<n; i++) {
+            mpp[nums[i]]++;
+        }
+
+        int ans = 0;
+        
+        for(auto it : mpp) {
+            if(it.second > (n / 2)) {
+                ans = it.first;
+            }
+        }
+        return ans;
+    }
+};
