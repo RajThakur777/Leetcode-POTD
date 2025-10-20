3e973f94a09e57cdf1cef15473f238296d4c316a
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-10-20 08:53:11 +0530

    Time: 4 ms (55.65%), Space: 15.1 MB (13.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,84 +1,19 @@
-//Brute force:
-// class Solution {
-// public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         vector<int> ans;
-//         int n = nums.size();
-
-//         for(int i=0; i<n; i++){
-//             for(int j=i+1; j<n; j++){
-//                 if(nums[i] + nums[j] == target){
-//                     ans.push_back(i);
-//                     ans.push_back(j);
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-
-
-
-
-
-//Better Approach:
-// class Solution {
-//  public:
-//     vector<int> twoSum(vector<int>& nums, int target) {
-//         int n = nums.size();
-
-//         sort(nums.begin() , nums.end());
-
-//         int i = 0;
-//         int j = n-1;
-
-//         vector<int> ans;
-
-//         while(i < j){
-//             int sum = nums[i] + nums[j];
-
-//             if(sum == target){
-//                 ans.push_back(i);
-//                 ans.push_back(j);
-//             }
-//             else if(sum > target){
-//                 j--;
-//             }
-//             else {
-//                 i++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-
-
-
-
-
-//Optimal Approach:
 class Solution {
- public:
+public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int , int> mpp;
+        int n = nums.size();
 
-        vector<int> ans;
+        map<int , int> mpp;
 
-        for(int i=0; i<nums.size(); i++){
-            int first = nums[i];
-            int second = target - first;
+        for(int i=0; i<n; i++) {
+            int ele = nums[i];
+            int need = target - ele;
 
-            if(mpp.find(second) != mpp.end()){
-                ans.push_back(i);
-                ans.push_back(mpp[second]);
-                break;
+            if(mpp.find(need) != mpp.end()) {
+                return {mpp[need] , i};
             }
-
-            mpp[first] = i;
+            mpp[ele] = i;
         }
-        return ans;
+        return {-1 , -1};
     }
 };
