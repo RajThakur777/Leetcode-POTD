90a7c011da3365b9d2bd4fbf4e4f2e3525ed4063
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-10-21 08:34:44 +0530

    Time: 4 ms (11.33%), Space: 71.8 MB (52.49%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -6,14 +6,12 @@ public:
         int ms = INT_MIN;
         int cs = 0;
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++) {
             cs += nums[i];
 
             ms = max(ms , cs);
             
-            if(cs < 0){
-                cs = 0;
-            }
+            if(cs < 0) cs = 0;
         }
         return ms;
     }
