7b76472bdc43fe77833c06ac39a6c25cc583402f
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-10-25 10:30:43 +0530

    Time: 3 ms (89.69%) | Memory: 23.9 MB (58.96%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,3 +1,33 @@
+// class Solution {
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         int n = intervals.size();
+
+//         sort(intervals.begin() , intervals.end());
+
+//         vector<vector<int>> ans;
+
+//         vector<int> prev = {intervals[0][0] , intervals[0][1]};
+
+//         for(int i=1; i<n; i++) {
+//             if(intervals[i][0] <= prev[1]) {
+//                 prev[0] = min(prev[0] , intervals[i][0]);
+//                 prev[1] = max(prev[1] , intervals[i][1]);
+//             }
+//             else {
+//                 ans.push_back(prev);
+//                 prev = {intervals[i][0] , intervals[i][1]};
+//             }
+//         }
+//         ans.push_back(prev);
+//         return ans;
+//     }
+// };
+
+
+
+
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
@@ -7,19 +37,14 @@ public:
 
         vector<vector<int>> ans;
 
-        vector<int> prev = {intervals[0][0] , intervals[0][1]};
-
-        for(int i=1; i<n; i++) {
-            if(intervals[i][0] <= prev[1]) {
-                prev[0] = min(prev[0] , intervals[i][0]);
-                prev[1] = max(prev[1] , intervals[i][1]);
+        for(int i=0; i<n; i++) {
+            if(ans.empty() || ans.back()[1] < intervals[i][0]) {
+                ans.push_back(intervals[i]);
             }
             else {
-                ans.push_back(prev);
-                prev = {intervals[i][0] , intervals[i][1]};
+                ans.back()[1] = max(ans.back()[1] , intervals[i][1]);
             }
         }
-        ans.push_back(prev);
         return ans;
     }
 };
