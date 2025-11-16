8cee60f7f31856289ddc29decb938b632c727467
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-11-16 09:41:12 +0530

    Time: 79 ms (25.00%) | Memory: 31.2 MB (8.33%) - LeetSync

---
+++ 4112-maximize-expression-of-three-elements/maximize-expression-of-three-elements.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximizeExpressionOfThree(vector<int>& nums) {
+        int n = nums.size();
+
+        int ans = INT_MIN;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                for(int k=0; k<n; k++) {
+                    if(i != j && j != k && i != k) {
+                        int v = nums[i] + nums[j] - nums[k];
+
+                        ans = max(ans , v);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
