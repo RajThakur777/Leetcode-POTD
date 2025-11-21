53543009ca8b15215629241d637c54835486dbaa
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-11-21 15:59:03 +0530

    Time: 395 ms (15.73%), Space: 16.2 MB (29.44%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        int cnt = 0;
-        
-        set<char> st;
-        for(int i=0; i<s.size(); i++){
+        int n = s.size();
+
+        set<int> st;
+        for(int i=0; i<n; i++) {
             st.insert(s[i]);
         }
 
-        for(auto it : st){
-            int i = -1;
-            int j = 0;
+        int ans = 0;
 
-            for(int k=0; k<s.size(); k++){
-                if(s[k] == it){
-                    if(i == -1){
-                        i = k;
-                    }
-                    else {
-                        j = k;
-                    }
+        for(auto it : st) {
+            int ele = it;
+            int l = -1;
+            int r = -1;
+
+            for(int j=0; j<n; j++) {
+                if(s[j] == ele) {
+                    l = j;
+                    break;
                 }
             }
 
-            set<char> ans;
-            for(int k=i+1; k<j; k++){
-                ans.insert(s[k]);
+            for(int j=n-1; j>=0; j--) {
+                if(s[j] == ele) {
+                    r = j;
+                    break;
+                }
             }
 
-            cnt += ans.size();
+            if(l != -1 && r != -1 && r - l > 1 && l != r) {
+                set<int> st2;
+                for(int k=l+1; k<=r-1; k++) {
+                    st2.insert(s[k]);
+                }
+
+                ans += st2.size();
+            }
         }
-        return cnt;
+        return ans;
     }
 };
