f26fad0fc7251f3d74071180e191cedefbc25b0a
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-12-04 23:47:07 +0530

    Time: 6 ms (8.95%), Space: 77.2 MB (44.26%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,38 +11,66 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+
         ListNode* temp1 = l1;
         ListNode* temp2 = l2;
 
         ListNode* dummy = new ListNode(-1);
-        ListNode* curr = dummy;
+        ListNode* t = dummy;
 
-        int carry = 0;
-
-        while(temp1 != nullptr || temp2 != nullptr || carry > 0){
-            int sum = carry;
-
-            if(temp1 != nullptr){
-                sum += temp1->val;
-            }
-            if(temp2 != nullptr){
-                sum += temp2->val;
-            }
-
+        while(temp1 != nullptr && temp2 != nullptr) {
+            int sum = temp1->val + temp2->val + carry;
             ListNode* newNode = new ListNode(sum % 10);
-            curr->next = newNode;
-            curr = curr->next;
+            t->next = newNode;
+            carry = (sum / 10);
+            t = newNode;
 
-            carry = sum / 10;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }   
 
-            if(temp1 != nullptr){
+        if(temp1 != nullptr) {
+            while(temp1 != nullptr) {
+                int sum = temp1->val + carry;
+                ListNode* newNode = new ListNode(sum % 10);
+                carry = (sum / 10);
+
+                t->next = newNode;
+                t = newNode;
+
                 temp1 = temp1->next;
             }
 
-            if(temp2 != nullptr){
-                temp2 = temp2->next;
+            if(carry > 0) {
+                t->next = new ListNode(carry);
+                t = t->next;
             }
         }
+        else if(temp2 != nullptr) {
+            while(temp2 != nullptr) {
+                int sum = temp2->val + carry;
+                ListNode* newNode = new ListNode(sum % 10);
+                carry = (sum / 10);
+
+                t->next = newNode;
+                t = newNode;
+
+                temp2 = temp2->next;
+            }
+
+            if(carry > 0) {
+                t->next = new ListNode(carry);
+                t = t->next;
+            }
+        }
+        else {
+            if(carry > 0) {
+                t->next = new ListNode(carry);
+                t = t->next;
+            }
+        }
+
         return dummy->next;
     }
 };
