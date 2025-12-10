3aacb9e6dbaab14ff5963e97b5457354de6cebbf
Raj Girdharsingh Thakur <traj15019@gmail.com>
2025-12-10 14:19:02 +0530

    Time: 3 ms (99.14%), Space: 11.6 MB (96.07%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -8,22 +8,20 @@
  */
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
+    bool hasCycle(ListNode *head) { 
         if(head == nullptr) return false;
-
-        if(head->next == nullptr) return false;
 
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast->next != nullptr && fast->next->next != nullptr) {
+        while(fast != nullptr && fast->next != nullptr) {  
             slow = slow->next;
             fast = fast->next->next;
 
             if(slow == fast) {
                 return true;
-            }
-        }
+            }     
+        }  
         return false;
     }
 };
